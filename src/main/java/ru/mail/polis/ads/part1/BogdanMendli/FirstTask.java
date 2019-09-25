a22521e40e9621044007ff995a808ec7eee38c9f
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-09-25 16:51:45 +0300

    rename class

--- src/main/java/ru/mail/polis/ads/part1/BogdanMendli/FirstTask.java
+++ src/main/java/ru/mail/polis/ads/part1/BogdanMendli/FirstTask.java
@@ -3,7 +3,7 @@ package ru.mail.polis.ads.part1.BogdanMendli;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class Main {
+public class FirstTask {
 
     private static void solve(Scanner in, PrintWriter out) {
         int result = in.nextInt();
