43e32253d0e9b2032cfec094f6f3ca100a5a5144
suhova <dasuhovada@yandex.ru>
2019-09-26 20:49:23 +0300

    success 1 and 6125

---
+++ src/main/java/ru/mail/polis/ads/part1/suhova/Task1.java
@@ -0,0 +1,43 @@
+package ru.mail.polis.ads.part1.suhova;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Task1 {
+    private static void solve(final Task1.FastScanner in, final PrintWriter out) {
+        // Write me
+       int x =  in.nextInt();
+       out.println((x/10)+" "+(x%10));
+       out.flush();
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final Task1.FastScanner in = new Task1.FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
