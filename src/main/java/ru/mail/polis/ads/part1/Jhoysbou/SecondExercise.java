7aaa68d8c4fe7016c65264926030b225ad0ea04b
Jhoysbou <neeroll21@gmail.com>
2019-09-26 21:40:59 +0300

    Uploading the third problem, that gets 83% of all tests.

--- src/main/java/ru/mail/polis/ads/part1/Jhoysbou/SecondExercise.java
+++ src/main/java/ru/mail/polis/ads/part1/Jhoysbou/SecondExercise.java
@@ -1,4 +1,21 @@
 package ru.mail.polis.ads.part1.Jhoysbou;
 
+import java.util.Scanner;
+
 public class SecondExercise {
+    public static void main(String... arg){
+
+    }
+
+    private static String makeSequance(String sequance){
+        int length = sequance.length();
+        String element = "";
+        for(int i = 0; i < length; ++i) {
+        }
+        return "";
+    }
+
+    private static boolean isCorrect(String sequance){
+    return false;
+    }
 }
---
+++ src/main/java/ru/mail/polis/ads/part1/Jhoysbou/ThirdExercise.java
@@ -0,0 +1,51 @@
+package ru.mail.polis.ads.part1.Jhoysbou;
+
+
+import java.util.Scanner;
+
+public class ThirdExercise {
+    public static void main(String... arg){
+        Scanner scanner = new Scanner(System.in);
+        String sequence = scanner.nextLine();
+        int length = sequence.length();
+
+        String[][] result = new String[length][length];
+
+        for (int len = 1; len <= length; ++len){
+            for (int left = 0; left + len -1 < length; ++left){
+
+                int right = left + len-1;
+                String min = sequence.substring(left, right + 1);
+                if (len > 4){
+                    for (int innerRight = left; innerRight < right; ++innerRight){
+                        int innerLeft = innerRight + 1;
+                        String temp = result[left][innerRight] + result[innerLeft][right];
+                        if (temp.length() < min.length()){
+                            min = temp;
+                        }
+                    }
+                    for (int factor = 1; factor < len; ++factor){
+                        if (len % factor == 0){
+                            boolean isPeriodic = true;
+                            for (int i = left + factor; i < right; ++i){
+                                if (sequence.charAt(i) != sequence.charAt(i - factor)){ isPeriodic = false; break; }
+                            }
+                            if (isPeriodic){
+                                String temp = len / factor + "(" + result[left][left + factor -1] + ")";
+                                if (temp.length() < min.length()){
+                                    min = temp;
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+                result[left][right] = min;
+                }
+            }
+        System.out.print(result[0][length - 1]);
+
+        }
+}
+
