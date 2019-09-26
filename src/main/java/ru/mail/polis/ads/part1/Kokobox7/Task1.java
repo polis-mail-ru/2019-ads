942ac6b4f20dfeafbfa64f15e4e01d590b9f046c
Saprykin Vitaliy <sapryckin.vitalij@yandex.ru>
2019-09-26 20:14:07 +0300

    Preparation

---
+++ src/main/java/ru/mail/polis/ads/part1/Kokobox7/Task1.java
@@ -0,0 +1,47 @@
+package ru.mail.polis.ads.part1.Kokobox7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public final class Task1 {
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        //int inNumber = in.next();
+    }
+
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
