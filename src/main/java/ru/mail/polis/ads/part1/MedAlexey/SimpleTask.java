650f70e9b9e677de1a544f17d2e7747edf4b8086
Alexey <medvedev.lasta@yandex.ru>
2019-09-27 22:09:48 +0700

    "simple task" implemented

---
+++ src/main/java/ru/mail/polis/ads/part1/MedAlexey/SimpleTask.java
@@ -0,0 +1,55 @@
+package ru.mail.polis.ads.part1.MedAlexey;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ *   Название задачи: "Простая задача?"
+ */
+public class SimpleTask {
+
+    private SimpleTask() {
+
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int inputData = in.nextInt();
+        out.write(inputData/10 + " " + inputData%10);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+
+}
