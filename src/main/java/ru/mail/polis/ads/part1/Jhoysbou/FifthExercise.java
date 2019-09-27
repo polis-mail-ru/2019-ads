1af2350995946c879a46113195e3865a988dbfc2
Jhoysbou <neeroll21@gmail.com>
2019-09-27 14:16:48 +0300

    Used BufferReader to boost performance. Still do not pass the last two tests.

--- src/main/java/ru/mail/polis/ads/part1/Jhoysbou/FifthExercise.java
+++ src/main/java/ru/mail/polis/ads/part1/Jhoysbou/FifthExercise.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.ads.part1.Jhoysbou;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Scanner;
 
 // Submission here https://www.e-olymp.com/ru/submissions/5723170
 
@@ -9,21 +11,26 @@ public class FifthExercise {
         SimpleQueue.run();
     }
 
-
     private static class SimpleQueue {
-        private static LinkedList<Integer> linkedList = new LinkedList<>();
+        private static LinkedList<Integer> arrayList = new LinkedList<>();
         private static boolean isRunning = false;
         private static String functionArgument;
 
 
         private static void run(){
             isRunning = true;
-            Scanner scanner = new Scanner(System.in);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
             while (isRunning){
-                functionArgument = scanner.nextLine();
+                try{
+                    functionArgument = reader.readLine();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
                 SimpleQueue.execute();
             }
-        }
+
+    }
 
         private static void execute(){
             int length = functionArgument.length();
@@ -42,26 +49,28 @@ public class FifthExercise {
 
             switch (command){
                 case "push":
-                    linkedList.add(commandArgument);
+                    arrayList.add(commandArgument);
                     System.out.println("ok");
                     break;
                 case "pop":
-                    System.out.println(linkedList.pop());
+                    System.out.println(arrayList.pop());
                     break;
                 case "front":
-                    System.out.println(linkedList.peekFirst());
+                    System.out.println(arrayList.peekFirst());
                     break;
                 case "size":
-                    System.out.println(linkedList.size());
+                    System.out.println(arrayList.size());
                     break;
                 case "clear":
-                    linkedList.clear();
+                    arrayList = new LinkedList<Integer>();
                     System.out.println("ok");
                     break;
                 case "exit":
                     isRunning = false;
                     System.out.println("bye");
                     break;
+                default:
+                    isRunning = false;
             }
         }
     }
