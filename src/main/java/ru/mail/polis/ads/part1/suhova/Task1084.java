e195924bacbcfea68e0989a9c48a1ac1416b52ff
suhova <dasuhovada@yandex.ru>
2019-09-30 01:53:14 +0300

    1084 97%

--- src/main/java/ru/mail/polis/ads/part1/suhova/Task1084.java
+++ src/main/java/ru/mail/polis/ads/part1/suhova/Task1084.java
@@ -1,48 +1,104 @@
 package ru.mail.polis.ads.part1.suhova;
+
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Task1084 {
+    private static int[][] len;
+
     private static void solve(final Task1084.FastScanner in, final PrintWriter out) {
-        char[] str = in.next().toCharArray();
-        Deque<Character> stack = new ArrayDeque<Character>();
-        StringBuilder res = new StringBuilder();
-        for(char c: str){
-            switch(c){
-                case '(':
-                case '[':
-                    stack.addLast(c);
-                    res.append(c);
-                    break;
-                case ')':
-                    if (!stack.isEmpty()&&stack.peekLast()=='('){
-                        stack.pollLast();
-                        res.append(c);
-                    } else {
-                        res.append('(');
-                        res.append(')');
-                    }
-                    break;
-                case ']':
-                    if (!stack.isEmpty()&&stack.peekLast()=='['){
-                        stack.pollLast();
-                        res.append(c);
-                    } else {
-                        res.append('[');
-                        res.append(']');
-                    }
-                    break;
+        String str = in.next();
+        len = new int[str.length()][str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            Arrays.fill(len[i], -1);
+        }
+        out.println(seqCreator(0, str.length() - 1, str));
+        out.flush();
+    }
+
+    private static String seqCreator(int l, int r, String str) {
+        if (l == r + 1) return "";
+        switch (str.charAt(l)) {
+            case ']':
+                return "[]" + seqCreator(l + 1, r, str);
+            case ')':
+                return "()" + seqCreator(l + 1, r, str);
+        }
+        switch (str.charAt(r)) {
+            case '[':
+                return seqCreator(l, r - 1, str) + "[]";
+            case '(':
+                return seqCreator(l, r - 1, str) + "()";
+        }
+        int ind = partitionIndex(l, r, str);
+        if (ind == -10) return str.charAt(l) + seqCreator(l + 1, r - 1, str) + str.charAt(r);
+        else return seqCreator(l, ind, str) + seqCreator(ind + 1, r, str);
+    }
+
+    private static int partitionIndex(int l, int r, String str) {
+        int minIndex = l;
+        int min = 2 + minLen(l + 1, r, str);
+        int sum = 2 + minLen(l, r - 1, str);
+        if (min > sum) {
+            min = sum;
+            minIndex = r-1;
+        }
+        for (int i = l + 1; i < r-1; i++) {
+            sum = minLen(l, i, str) + minLen(i + 1, r, str);
+            if (min > sum) {
+                min = sum;
+                minIndex = i;
             }
         }
-        while(!stack.isEmpty()){
-            if(stack.pollLast()=='(') res.append(')');
-            else res.append(']');
+        len[l][r] = min;
+        if (str.charAt(l) == '(' && str.charAt(r) == ')' || str.charAt(l) == '[' && str.charAt(r) == ']') {
+            int x = minLen(l + 1, r - 1, str) + 2;
+            if (x < len[l][r]) {
+                len[l][r] = x;
+                minIndex = -10;
+            }
         }
-        String s = new String(res);
-        out.println(s);
-        out.flush();
+        return minIndex;
+    }
+
+    private static int minLen(int l, int r, String str) {
+        if (l == r) {
+            len[l][r] = 2;
+            return 2;
+        }
+        if (l > r) {
+            len[l][r] = 0;
+            return 0;
+        }
+        if (len[l][r] == -1) {
+            if (str.charAt(l) == ')' || str.charAt(l) == ']') {
+                len[l][r] = 2 + minLen(l + 1, r, str);
+            } else if (str.charAt(r) == '(' || str.charAt(r) == '[') {
+                len[l][r] = 2 + minLen(l, r - 1, str);
+            } else {
+                int min = 2 + minLen(l + 1, r, str);
+                int sum = 2 + minLen(l, r - 1, str);
+                if (min > sum) min = sum;
+                for (int i = l + 1; i < r; i++) {
+                    sum = minLen(l, i, str) + minLen(i + 1, r, str);
+                    if (min > sum) min = sum;
+                }
+                len[l][r] = min;
+                if (str.charAt(l) == '(' && str.charAt(r) == ')' || str.charAt(l) == '[' && str.charAt(r) == ']') {
+                    int x = minLen(l + 1, r - 1, str) + 2;
+                    if (x < len[l][r]) len[l][r] = x;
+                }
+            }
+        }
+        return len[l][r];
+    }
+
+    public static void main(final String[] arg) {
+        final Task1084.FastScanner in = new Task1084.FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
     }
 
     private static class FastScanner {
@@ -63,16 +119,8 @@ public class Task1084 {
             }
             return tokenizer.nextToken();
         }
-
         int nextInt() {
             return Integer.parseInt(next());
-        }
-    }
-
-    public static void main(final String[] arg) {
-        final Task1084.FastScanner in = new Task1084.FastScanner(System.in);
-        try (PrintWriter out = new PrintWriter(System.out)) {
-            solve(in, out);
         }
     }
 }
