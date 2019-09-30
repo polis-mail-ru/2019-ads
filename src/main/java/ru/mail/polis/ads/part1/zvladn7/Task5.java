65412e86bdaf634e5518858fbdaceebe5de3d1e4
Vlad Zybkin <zybkin11@gmail.com>
2019-09-30 18:23:34 +0300

    code climate changes Task5

--- src/main/java/ru/mail/polis/ads/part1/zvladn7/Task5.java
+++ src/main/java/ru/mail/polis/ads/part1/zvladn7/Task5.java
@@ -1,14 +1,17 @@
 package ru.mail.polis.ads.part1.zvladn7;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Task5 {
-  public static void solve(BufferedReader in, PrintWriter out) {
-    LinkedList<Integer> queue = new LinkedList<>();
+  public static void solve(final BufferedReader in, final PrintWriter out) {
+    final LinkedList<Integer> queue = new LinkedList<>();
     try {
       String[] str = in.readLine().split(" ");
-      out:
-      while (true) {
+      while (!"exit".equals(str[0])) {
         switch (str[0]) {
           case "push": {
             queue.addLast(Integer.parseInt(str[1]));
@@ -32,20 +35,20 @@ public class Task5 {
             out.println("ok");
             break;
           }
-          case "exit": {
-            out.println("bye");
-            break out;
+          default: {
+            throw new IllegalArgumentException("I've just sent the wrong command!");
           }
         }
         str = in.readLine().split(" ");
       }
+      out.println("bye");
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  public static void main(String[] args) {
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(final String[] args) {
+    final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     try (PrintWriter out = new PrintWriter(System.out)) {
       solve(in, out);
     }
