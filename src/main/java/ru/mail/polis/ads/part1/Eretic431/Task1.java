2cba5a93907a078e139374e1e1bc3bb7d9f78f6f
Eretic431 <abdullapower@outlook.com>
2019-10-01 17:47:18 +0300

    Task1 solved

---
+++ src/main/java/ru/mail/polis/ads/part1/Eretic431/Task1.java
@@ -0,0 +1,17 @@
+package main.java.ru.mail.polis.ads.part1.Eretic431;
+
+import java.util.Scanner;
+
+/**
+ * https://www.e-olymp.com/ru/submissions/5757660
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        final Scanner in = new Scanner(System.in);
+        final int n = in.nextInt();
+        final String tmp = String.valueOf(n);
+
+        System.out.println(tmp.charAt(0) + " " + tmp.charAt(1));
+    }
+}
