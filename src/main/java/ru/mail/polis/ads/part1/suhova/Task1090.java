308172847745ab12300ff153ea9d8d90b021eb99
suhova <dasuhovada@yandex.ru>
2019-10-03 17:41:53 +0300

    done

---
+++ src/main/java/ru/mail/polis/ads/part1/suhova/Task1090.java
@@ -0,0 +1,87 @@
+package ru.mail.polis.ads.part1.suhova;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Task1090 {
+    /*
+    https://www.e-olymp.com/ru/submissions/5756855
+     */
+    private static String[][] res;
+
+    private static String packing(String str, String sub, int length, int l, int r) {
+        for (int i = 1; i <= length / 2; i++) {
+            int count = length / i;
+            if (length % i == 0) {
+                for (int j = l; j <= r - i; j++) {
+                    if (str.charAt(j) != str.charAt(j + i)) {
+                        count = 0;
+                        break;
+                    }
+                }
+            } else count = 0;
+            if (count > 1) {
+                String s = length / i + "(" + res[l][l + i - 1] + ")";
+                if (s.length() < sub.length())
+                    sub = s;
+            }
+        }
+        return sub;
+    }
+
+    private static void solve(final Scanner in, final PrintWriter out) {
+        String str = in.nextLine();
+        int len = str.length();
+        res = new String[len][len];
+        for (int n = 1; n <= len; n++) {
+            for (int l = 0; l <= len - n; l++) {
+                int r = n + l - 1;
+                String sub = str.substring(l, r + 1);
+                if (n > 4) {
+                    sub = packing(str, sub, n, l, r);
+                    for (int part = l; part < r; part++) {
+                        String newStr = res[l][part] + res[part + 1][r];
+                        if (newStr.length() < sub.length()) {
+                            sub = newStr;
+                        }
+                    }
+                }
+                res[l][r] = sub;
+            }
+        }
+        out.println(res[0][len - 1]);
+        out.flush();
+    }
+
+    public static void main(final String[] arg) {
+        final Scanner in = new Scanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
---
+++ src/main/java/ru/mail/polis/ads/part1/suhova/Task1618.java
@@ -0,0 +1,75 @@
+package ru.mail.polis.ads.part1.suhova;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Task1618 {
+    /*
+    https://www.e-olymp.com/ru/submissions/5735270
+     */
+    private static void solve(final Task1618.FastScanner in, final PrintWriter out) {
+        int n1 = in.nextInt();
+        int[] arr1 = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = in.nextInt();
+        }
+        int n2 = in.nextInt();
+        int[] arr2 = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = in.nextInt();
+        }
+        int[][] map = new int[n1][n2];
+        for (int i = 0; i < n1; i++) {
+            Arrays.fill(map[i], -1);
+        }
+        int count = maxSub(n1 - 1, n2 - 1, arr1, arr2, map);
+        out.println(count);
+        out.flush();
+    }
+
+    private static int maxSub(int i, int j, int[] a, int[] b, int[][] map) {
+        if (i == -1 || j == -1)
+            return 0;
+        if (a[i] == b[j]) {
+            if (i == 0 || j == 0) return maxSub(i - 1, j - 1, a, b, map) + 1;
+            if (map[i - 1][j - 1] == -1) map[i - 1][j - 1] = maxSub(i - 1, j - 1, a, b, map);
+            return map[i - 1][j - 1] + 1;
+        }
+        if (i == 0 || j == 0) return 0;
+        if (map[i][j - 1] == -1) map[i][j - 1] = maxSub(i, j - 1, a, b, map);
+        if (map[i - 1][j] == -1) map[i - 1][j] = maxSub(i - 1, j, a, b, map);
+        return Math.max(map[i][j - 1], map[i - 1][j]);
+    }
+
+    public static void main(final String[] arg) {
+        final Task1618.FastScanner in = new Task1618.FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
