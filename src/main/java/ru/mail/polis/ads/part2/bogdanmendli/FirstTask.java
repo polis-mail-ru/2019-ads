710d486f44ab5dcf3d49e9f5dd43ad407f5bbe11
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-10-04 16:42:05 +0300

    refactoring first task

--- src/main/java/ru/mail/polis/ads/part2/bogdanmendli/FirstTask.java
+++ src/main/java/ru/mail/polis/ads/part2/bogdanmendli/FirstTask.java
@@ -1,14 +1,18 @@
 package ru.mail.polis.ads.part2.bogdanmendli;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class FirstTask {
 
     private static int[][] paths;
+    private static Deque<Character> result;
 
     private static void solve(final Scanner in) {
         final int m = in.nextInt();
         final int n = in.nextInt();
+        result = new ArrayDeque<>();
         paths = new int[m][n];
         int[][] grains = new int[m][n];
 
@@ -34,44 +38,39 @@ public class FirstTask {
                 paths[i][j] = grains[i][j] + paths[i + 1][j];
             }
         }
-
-        System.out.println(restorePath(m, n));
+        restorePath(m, n);
+        print();
     }
 
-    private static String restorePath(final int m, final int n) {
-        String result = "";
+    private static void restorePath(final int m, final int n) {
         int right = n - 1;
         int front = 0;
         while (right != 0 && front != m - 1) {
             if (paths[front][right - 1] > paths[front + 1][right]) {
-                result += "R";
+                result.push('R');
                 right--;
                 continue;
             }
-            result += "F";
+            result.push('F');
             front++;
         }
         if (front == m - 1) {
             while (right != 0) {
                 right--;
-                result += "R";
+                result.push('R');
             }
         } else {
             while (front != m - 1) {
                 front++;
-                result += "F";
+                result.push('F');
             }
         }
-
-        return reversePath(result);
     }
 
-    private static String reversePath(String path) {
-        String temp = "";
-        for (int i = 0; i < path.length(); i++) {
-            temp += path.charAt(path.length() - i - 1);
+    public static void print() {
+        while (!result.isEmpty()) {
+            System.out.print(result.pop());
         }
-        return temp;
     }
 
     public static void main(String[] args) {
--- src/main/java/ru/mail/polis/ads/part2/bogdanmendli/ThirdTask.java
+++ src/main/java/ru/mail/polis/ads/part2/bogdanmendli/ThirdTask.java
@@ -1,48 +1,51 @@
 package ru.mail.polis.ads.part2.bogdanmendli;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class ThirdTask {
 
-//    private static int index;
-//    private static String result[];
-//    private static char expression[];
-//
-//    private static void fillResult(final int level) {
-//        result[level] += expression[index];
-//        index--;
-//        if (Character.isUpperCase(expression[index + 1])) {
-//            fillResult(level + 1);
-//            fillResult(level + 1);
-//        }
-//    }
-//
-//    private static void solve(){
-//        Scanner in = new Scanner(System.in);
-//        int t = in.nextInt();
-//        String str;
-//        for (int i = 0; i < t; i++) {
-//            str = in.next();
-//            expression = str.toCharArray();
-//            result = new String[expression.length];
-//            Arrays.fill(result, "");
-//            index = expression.length - 1;
-//            fillResult(0);
-//            print(expression.length - 1, result);
-//        }
-//    }
-//
-//    private static void print(final int length, String[] result) {
-//        for (int i = length; i >= 0; i--) {
-//            System.out.print(result[i]);
-//        }
-//        System.out.println();
-//    }
-//
-//    public static void main(String[] args) {
-//        solve();
-//    }
+    private static int index;
+    private static String[] result;
+    private static char[] expression;
 
+    private static void solve(){
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            int t = Integer.parseInt(in.readLine());
+            for (int i = 0; i < t; i++) {
+                final String str = in.readLine();
+                expression = str.toCharArray();
+                result = new String[expression.length];
+                Arrays.fill(result, "");
+                index = expression.length - 1;
+                fillResult(0);
+                print();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static void fillResult(final int level) {
+        result[level] += expression[index];
+        index--;
+        if (Character.isUpperCase(expression[index + 1])) {
+            fillResult(level + 1);
+            fillResult(level + 1);
+        }
+    }
+
+    private static void print() {
+        for (int i = result.length - 1; i >= 0; i--) {
+            System.out.print(result[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
 }
