523a38915a74ccfbd516322c39a03a08cb2e4631
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-10-06 20:57:01 +0300

    Created empty tasks

---
+++ src/main/java/ru/mail/polis/ads/part2/maksimshengeliia/Task5.java
@@ -0,0 +1,45 @@
+package ru.mail.polis.ads.part2.maksimshengeliia;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Task5 {
+    private Task5() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        // Write me
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
