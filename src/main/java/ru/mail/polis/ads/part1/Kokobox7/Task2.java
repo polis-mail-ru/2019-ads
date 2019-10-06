864e70a3719aa98e55fef2d309c1511c358d5abf
Saprykin Vitaly <sapryk1n.vitaliy@yandex.ru>
2019-10-06 13:37:12 +0300

    Task 1,2 completed.

--- src/main/java/ru/mail/polis/ads/part1/Kokobox7/Task2.java
+++ src/main/java/ru/mail/polis/ads/part1/Kokobox7/Task2.java
@@ -1,105 +1,80 @@
 package ru.mail.polis.ads.part1.Kokobox7;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public final class Task2 {
 
-    private static void solve(final FastScanner in, final PrintWriter out) {
-        String input = in.next();
-        if (input.isEmpty())
-            out.print("");
-        //index of the encountered opening parenthesis - (
-        int ixOpeningParenthesis = -1;
-        int ixOpeningBracket = -1;
-        StringBuilder outputBuilder = new StringBuilder();
-        outputBuilder.append(input);
+    private static void solve(final Scanner in, final PrintWriter out) {
+        String s = in.nextLine();
+        if (s.isEmpty()){
+            out.println();
+            return;
+        }
+        int n = s.length();
+        int[][] d = new int[n][n];
+        int[][] split = new int[n][n];
 
-        char cur_char;
-        int offset = 1;
-        for (int i = 0; i < outputBuilder.length(); i++) {
-            cur_char = outputBuilder.charAt(i);
-            switch (cur_char) {
-                case '(':
-                    if (ixOpeningParenthesis == -1) {
-                        ixOpeningParenthesis = i;
-                        //outputBuilder.append('(');
-                    } else {
-                        outputBuilder.insert(ixOpeningParenthesis + 1, ")");
-                        //outputBuilder.insert(outputBuilder.lastIndexOf("(") + 1, ")");
-                        //offset += 1;
-                        //outputBuilder.append('(');
-                        ixOpeningParenthesis = i;
+        for (int j = 0; j < n; j++) {
+            for (int i = j; i >= 0; i--) {
+                if (i == j) {
+                    d[i][j] = 1;
+                    continue;
+                }
+                int min = Integer.MAX_VALUE;
+                int splitMin = -1;
+                if ((s.charAt(i) == '(' && s.charAt(j) == ')') ||
+                        (s.charAt(i) == '[' && s.charAt(j) == ']')) {
+                    min = d[i + 1][j - 1];
+                }
+                for (int k = i; k < j; k++) {
+                    if (d[i][k] + d[k + 1][j] < min) {
+                        min = d[i][k] + d[k + 1][j];
+                        splitMin = k;
                     }
-                    break;
-                case ')':
-                    if (ixOpeningParenthesis == -1) {
-                        //outputBuilder.append("(");
-                        outputBuilder.insert(i, "(");
-                    } else {
-                        //outputBuilder.append(')');
-                        ixOpeningParenthesis = -1;
-                    }
-                    break;
-                case '[':
-                    if (ixOpeningBracket == -1) {
-                        ixOpeningBracket = i;
-                        outputBuilder.append('[');
-                    } else {
-                        outputBuilder.insert(ixOpeningBracket + 1, "]");
-                        //outputBuilder.insert(outputBuilder.indexOf("[", i) + 1, "]");
-                        offset += 1;
-                        outputBuilder.append('(');
-                        ixOpeningBracket = i;
-                    }
-                    break;
-                case ']':
-                    if (ixOpeningBracket == -1) {
-                        //outputBuilder.append("[]");
-                        outputBuilder.insert(i, "[");
-                    } else {
-                        //outputBuilder.append(']');
-                        ixOpeningBracket = -1;
-                    }
-                    break;
+                }
+                d[i][j] = min;
+                split[i][j] = splitMin;
             }
         }
-        if (ixOpeningParenthesis != -1) {
-            outputBuilder.insert(outputBuilder.lastIndexOf("(") + 1, ")");
-        }
-        if (ixOpeningBracket != -1) {
-            outputBuilder.insert(outputBuilder.lastIndexOf("[") + 1, "]");
-        }
-        out.print(outputBuilder.toString());
+        //System.out.println(d[0][n - 1]);
+        restore(0, n - 1, s, d, split);
     }
 
-
-    private static class FastScanner {
-        private final BufferedReader reader;
-        private StringTokenizer tokenizer;
-
-        FastScanner(final InputStream in) {
-            reader = new BufferedReader(new InputStreamReader(in));
-        }
-
-        String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    static void restore(int i, int j,
+                        String s,
+                        int[][] d, int[][] split) {
+        if (i == j) {
+            switch (s.charAt(i)) {
+                case '(':
+                case ')':
+                    System.out.print("()");
+                    break;
+                case ']':
+                case '[':
+                    System.out.print("[]");
+                    break;
             }
-            return tokenizer.nextToken();
+            return;
         }
-
-        int nextInt() {
-            return Integer.parseInt(next());
+        if (d[i][j] == 0) {
+            System.out.print(s.substring(i, j + 1));
+            return;
         }
+        if (split[i][j] == -1) {
+            System.out.print(s.charAt(i));
+            restore(i + 1, j - 1, s, d, split);
+            System.out.print(s.charAt(j));
+            return;
+        }
+        int k = split[i][j];
+        restore(i, k, s, d, split);
+        restore(k + 1, j, s, d, split);
     }
 
     public static void main(final String[] arg) {
-        final FastScanner in = new FastScanner(System.in);
+        final Scanner in = new Scanner(System.in);
         try (PrintWriter out = new PrintWriter(System.out)) {
             solve(in, out);
         }
---
+++ src/main/java/ru/mail/polis/ads/part1/Kokobox7/Task5.java
@@ -0,0 +1,54 @@
+package ru.mail.polis.ads.part1.Kokobox7;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+
+public final class Task5 {
+    public static class MyQueue {
+        LinkedList q = new LinkedList();
+
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        MyQueue q = new MyQueue();
+
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
