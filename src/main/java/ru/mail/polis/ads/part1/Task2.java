8dc15f451871449b8105e8b646bec34fb89c86d8
igorlaryushin <igorlaryush@yandex.ru>
2019-10-07 16:49:52 +0300

    homeWork1

--- src/main/java/ru/mail/polis/ads/part1/Task2.java
+++ src/main/java/ru/mail/polis/ads/part1/Task2.java
@@ -2,56 +2,70 @@ package ru.mail.polis.ads.part1;
 
 import java.util.Scanner;
 
+
 public class Task2 {
-    private static String string;
-    private static String[][] packed;
-
-    private Task2() {
-    }
-
-    private static void solve() {
-        final Scanner scanner = new Scanner(System.in);
-        string = scanner.nextLine();
-        final int length = string.length();
-        packed = new String[length][length];
-        for (int len = 1; len <= length; len++) {
-            for (int left = 0; left + len - 1 < length; left++) {
-                int right = left + len - 1;
-                String min = string.substring(left, right + 1);
-                if (len > 4) {
-                    for (int right1 = left; right1 < right; right1++) {
-                        int left2 = right1 + 1;
-                        String t = "";
-                        t = packed[left][right1] + packed[left2][right];
-                        if (t.length() < min.length()) {
-                            min = t;
-                        }
-                    }
-                    for (int period = 1; period < len; period++) {
-                        if (len % period == 0) {
-                            boolean isPeriod = true;
-                            for (int i = left + period; i <= right; i++) {
-                                if (string.charAt(i) != string.charAt(i - period)) {
-                                    isPeriod = false;
-                                    break;
-                                }
-                            }
-                            if (isPeriod) {
-                                String t = len / period + "(" + packed[left][left + period - 1] + ")";
-                                if (t.length() < min.length()) {
-                                    min = t;
-                                }
-                            }
-                        }
-                    }
-                }
-                packed[left][right] = min;
-            }
-        }
-        System.out.println(packed[0][length - 1]);
-    }
 
     public static void main(String[] args) {
-        solve();
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (s.isEmpty()) {
+            return;
+        }
+        int n = s.length();
+        int[][] d = new int[n][n];
+        int[][] split = new int[n][n];
+        for (int j = 0; j < n; j++) {
+            for (int i = j; i >= 0; i--) {
+                if (i == j) {
+                    d[i][j] = 1;
+                    continue;
+                }
+                int min = Integer.MAX_VALUE;
+                int splitMin = -1;
+                if (s.charAt(i) == '(' && s.charAt(j) == ')' ||
+                        s.charAt(i) == '[' && s.charAt(j) == ']') {
+                    min = d[i + 1][j - 1];
+                }
+                for (int k = i; k < j; k++) {
+                    if (d[i][k] + d[k + 1][j] < min) {
+                        min = d[i][k] + d[k + 1][j];
+                        splitMin = k;
+                    }
+                }
+                d[i][j] = min;
+                split[i][j] = splitMin;
+            }
+        }
+        //System.out.println(d[0][n - 1]);
+        restore(0, n-1, s, d, split);
+    }
+
+    static void restore(int i,int j,String s,int[][]d,int[][]split){
+        if(i == j){
+            switch(s.charAt(i)){
+                case'(':
+                case')':
+                    System.out.print("()");
+                    break;
+                case'[':
+                case']':
+                    System.out.print("[]");
+                    break;
+            }
+            return;
+        }
+        if(d[i][j] == 0){
+            System.out.print(s.substring(i,j+1));
+            return;
+        }
+        if(split[i][j] == -1){
+            System.out.print(s.charAt(i));
+            restore(i+1,j-1,s,d,split);
+            System.out.print(s.charAt(j));
+            return;
+        }
+        int k=split[i][j];
+        restore(i,k,s,d,split);
+        restore(k+1,j,s,d,split);
     }
 }
