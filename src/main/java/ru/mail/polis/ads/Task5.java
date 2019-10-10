07a25fed375f40c6c4e557b0f4eadcdd46ffa922
gogun <gusarov2000@gmail.com>
2019-10-10 19:21:13 +0300

    add Fifth task

---
+++ src/main/java/ru/mail/polis/ads/Task5.java
@@ -0,0 +1,54 @@
+package ru.mail.polis.ads;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Task5 {
+    public static void solve(BufferedReader bufferedReader) throws IOException {
+        String input = "";
+        LinkedList<String> linkedList = new LinkedList<>();
+        while (!input.equals("exit")) {
+            input = bufferedReader.readLine();
+            switch (input.split(" ")[0]) {
+                case ("push"):
+                    linkedList.push(input.split(" ")[1]);
+                    System.out.println("ok");
+                    break;
+                case ("pop"):
+                    if (linkedList.size() != 0) {
+                        System.out.println(linkedList.pop());
+                    } else {
+                        System.out.println("error");
+                    }
+                    break;
+                case ("back"):
+                    if (linkedList.size() != 0) {
+                        System.out.println(linkedList.getFirst());
+                    } else {
+                        System.out.println("error");
+                    }
+                    break;
+                case ("size"):
+                    System.out.println(linkedList.size());
+                    break;
+                case ("clear"):
+                    linkedList.clear();
+                    System.out.println("ok");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            solve(bufferedReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("bye");
+    }
+}
+
