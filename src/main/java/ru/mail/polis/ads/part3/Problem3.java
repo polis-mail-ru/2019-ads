bbc05400870dc013a06320636686069657c63e61
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-10-10 12:57:00 +0300

    Problem 3

---
+++ src/main/java/ru/mail/polis/ads/part3/Problem3.java
@@ -0,0 +1,56 @@
+package ru.mail.polis.ads.part3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+/**
+ * submission - https://www.e-olymp.com/ru/submissions/5822963
+ */
+public final class Problem3 {
+
+    private Problem3() {
+        // Should not be instantiated
+    }
+
+    private static void solve() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+
+        String[] tokens = reader.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(tokens[i]);
+        }
+
+        int swaps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            boolean isSwapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    swaps++;
+                    isSwapped = true;
+                }
+            }
+            if (!isSwapped) {
+                break;
+            }
+        }
+        System.out.println(swaps);
+    }
+
+    public static void main(final String[] arg) {
+        try {
+            solve();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
