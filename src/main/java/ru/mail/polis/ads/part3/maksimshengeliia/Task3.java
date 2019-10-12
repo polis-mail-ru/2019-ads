875b3763c8a5168fab7ba1613aa80320a232e54c
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-10-12 20:47:03 +0300

    added empty tasks for part3

--- src/main/java/ru/mail/polis/ads/part3/maksimshengeliia/Task3.java
+++ src/main/java/ru/mail/polis/ads/part3/maksimshengeliia/Task3.java
@@ -1,4 +1,45 @@
 package ru.mail.polis.ads.part3.maksimshengeliia;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Task3 {
+    private Task3() {
+        // Should not be instantiated
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        // Write me
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
 }
