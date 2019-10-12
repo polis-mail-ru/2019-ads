a7fa281db0b47011b82ae250d3ea58dde2132f9b
NastyPill <DJPonnYash.Vinyl@gmail.com>
2019-10-13 01:24:08 +0300

    3 tasks done

--- src/ru/mail/polis/ads/part1/nastypill/Task1618.java
+++ src/ru/mail/polis/ads/part1/nastypill/Task1618.java
@@ -13,28 +13,36 @@ import java.util.stream.Collectors;
  */
 public final class Task1618 {
     private Task1618() {
-        // Should not be instantiated
+        // Should not be instantiated https://www.e-olymp.com/ru/submissions/5804738
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        ArrayList<Integer> list1 = new ArrayList<>(readList(in));
-        ArrayList<Integer> list2 = new ArrayList<>(readList(in));
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(0);
+        list1.addAll(readList(in));
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(0);
+        list2.addAll(readList(in));
         int[][] d = new int[2][Math.max(list1.size() + 1, list2.size() + 1)];
-        for (int i = 1; i <= list1.size(); i++) {
-            for (int j = 1; j <= list2.size(); j++) {
-                if(list1.get(i - 1) == list2.get(j - 1)) {
+        for (int i = 1; i < list1.size(); i++) {
+            for (int j = 1; j < list2.size(); j++) {
+                if(list1.get(i).equals(list2.get(j))) {
                     d[i % 2][j] = d[(i + 1) % 2][j - 1] + 1;
                 } else {
                     d[i % 2][j] = Math.max(d[(i + 1) % 2][j], d[i % 2][j - 1]);
                 }
             }
         }
-        out.println(d[list1.size() % 2][list2.size()]);
+        out.println(d[(list1.size() - 1) % 2][list2.size() - 1]);
     }
 
     private static List<Integer> readList(final FastScanner in) {
-        in.nextInt();
-        return Arrays.stream(in.next().split(" +")).map(Integer::parseInt).collect(Collectors.toList());
+        int n  = in.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(in.nextInt());
+        }
+        return list;
     }
 
     private static class FastScanner {
@@ -48,7 +56,7 @@ public final class Task1618 {
         String next() {
             while (tokenizer == null || !tokenizer.hasMoreTokens()) {
                 try {
-                    tokenizer = new StringTokenizer(reader.readLine(), "\n");
+                    tokenizer = new StringTokenizer(reader.readLine());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
