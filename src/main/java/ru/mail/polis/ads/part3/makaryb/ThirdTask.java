c9723fcd621ec88a1664608c4116858241a925e8
Makary <makary.b@gmail.com>
2019-10-13 20:15:00 +0300

    Добавляет решение третьей задачи третьего дз

---
+++ src/main/java/ru/mail/polis/ads/part3/makaryb/ThirdTask.java
@@ -0,0 +1,73 @@
+package ru.mail.polis.ads.part3.makaryb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Made by БорискинМА
+ * 13.10.19
+ * gr. Java-10, Технополис
+ * IntelliJ IDEA Ultimate 2019.2 (JetBrains Product Pack for Students)
+ * e-olymp 100%: https://www.e-olymp.com/ru/submissions/5849739
+ */
+public final class ThirdTask {
+    private ThirdTask() {}
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int result = 0;
+        int n = in.nextInt();
+        Integer[] array = new Integer[n+1];
+
+        for (int i = 1; i < n+1; i++) {
+            array[i] = in.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    result++;
+                }
+            }
+        }
+
+        out.println(result);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
