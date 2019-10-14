033aa7e4157deaacf821c17e9e02eea76accccda
Saprykin Vitaly <sapryk1n.vitaliy@yandex.ru>
2019-10-14 21:12:23 +0300

    Completed all tasks. (5/5)

--- src/main/java/ru/mail/polis/ads/part2/kokobox7/Ladder.java
+++ src/main/java/ru/mail/polis/ads/part2/kokobox7/Ladder.java
@@ -4,8 +4,34 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Ladder {
+
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        int[] ladder = new int[n + 2];
+        ladder[0] = 0;
+        ladder[n + 1] = 0;
+        for (int i = 1; i <= n; i++) {
+            ladder[i] = in.nextInt();
+        }
+        int k = in.nextInt();
+        //System.out.println(Arrays.toString(ladder));
+
+        int[] dp = ladder.clone();
+        for (int j = 1; j <= n + 1; j++) {
+            initDP(k, dp, j);
+        }
+        //System.out.println(Arrays.toString(dp));
+        out.println(dp[n + 1]);
+    }
+
+    private static void initDP(int k, int[] dp, int j) {
+        int left = Math.max(j - k, 0);
+        int right = j;
+        int max = dp[left];
+        for (int i = left; i < right; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        dp[j] += max;
     }
 
     private static class FastScanner {
