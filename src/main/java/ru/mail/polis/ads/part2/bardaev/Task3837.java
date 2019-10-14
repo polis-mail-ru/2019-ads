6e3c7eae598cb1df410f9134235f7d43ec6f13c3
Badma <48029071+0b010101@users.noreply.github.com>
2019-10-15 00:56:47 +0300

    part2 full

--- src/main/java/ru/mail/polis/ads/part2/bardaev/Task3837.java
+++ src/main/java/ru/mail/polis/ads/part2/bardaev/Task3837.java
@@ -5,13 +5,100 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Task3837 {
     private Task3837() {}
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        String result = "";
+        int a = in.nextInt();
+        ArrayDeque<String> expressions = new ArrayDeque<>();
+        for (int i = 0; i < a; i++) {
+            expressions.addLast(in.next());
+        }
+
+        MyStack[] res = new MyStack[a];
+        for (int i = 0; i < a; i++) {
+            MyStack stack = new MyStack();
+            String str = expressions.poll();
+            for (int j = 0; j < str.length(); j++) {
+                if (Character.isLowerCase(str.charAt(j))) {
+                    MyQueue m = new MyQueue();
+                    m.expr = String.valueOf(str.charAt(j));
+                    stack.push(m);
+                } else if (Character.isUpperCase(str.charAt(j))) {
+                    MyQueue q = new MyQueue();
+                    q.expr = String.valueOf(str.charAt(j));
+                    q.rightVal = stack.poll();
+                    q.leftVal = stack.poll();
+                    stack.push(q);
+                }
+            }
+            res[i] = stack;
+            stack = null;
+        }
+
+        ArrayDeque<MyQueue> arr = new ArrayDeque<>();
+        for (int i = 0; i < res.length; i++) {
+            MyStack st = res[i];
+            arr.addLast(st.poll());
+            while (arr.size() > 0) {
+                MyQueue q = arr.poll();
+                result += q.expr;
+                if (!(q.rightVal == null && q.leftVal == null)) {
+                    arr.addLast(q.leftVal);
+                    arr.addLast(q.rightVal);
+                }
+            }
+
+            StringBuffer s = new StringBuffer(result);
+            out.println(s.reverse());
+            result = "";
+            out.flush();
+        }
+
+    }
+
+    private static class MyQueue {
+        String expr;
+        MyQueue leftVal;
+        MyQueue rightVal;
+        MyQueue () {
+            this.expr = null;
+            this.leftVal = null;
+            this.rightVal = null;
+        }
+    }
+
+    private static class MyStack {
+        private int count;
+        private ArrayList<MyQueue> arr;
+
+        MyStack() {
+            this.count = 0;
+            this.arr = new ArrayList<>();
+        }
+
+        public void push(MyQueue s) {
+            arr.add(s);
+            count++;
+        }
+
+        public MyQueue poll() {
+            if (count > 0) {
+                MyQueue temp = arr.remove(count-1);
+                count--;
+                return temp;
+            }
+            return null;
+        }
+
+        public int size() {
+            return count;
+        }
     }
 
     private static class FastScanner {
