46d33972c7222577d5d69ce767dc3094117e647a
Eretic431 <abdullapower@outlook.com>
2019-10-14 22:04:31 +0300

    Task2

---
+++ src/main/java/ru/mail/polis/ads/part1/Eretic431/Task2.java
@@ -0,0 +1,77 @@
+package main.java.ru.mail.polis.ads.part1.Eretic431;
+
+import java.util.Scanner;
+
+/**
+ * https://www.e-olymp.com/ru/submissions/5858002
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = s.length();
+        int[][] d = new int[n][n];
+        int[][] split = new int[n][n];
+
+        for (int j = 0; j < n; j++) {
+            for (int i = j; i >= 0; i--) {
+                if (i == j) {
+                    d[i][j] = 1;
+                    continue;
+                }
+
+                int min = Integer.MAX_VALUE;
+                int splitMin = -1;
+
+                if (s.charAt(i) == '(' && s.charAt(j) == ')' ||
+                    s.charAt(i) == '[' && s.charAt(j) == ']') {
+                    min = d[i + 1][j - 1];
+                }
+
+                for (int k = i; k < j; k++) {
+                    if (d[i][k] + d[k + 1][j] < min) {
+                        min = d[i][k] + d[k + 1][j];
+                        splitMin = k;
+                    }
+                }
+
+                d[i][j] = min;
+                split[i][j] = splitMin;
+            }
+        }
+
+        restore(0, n - 1, s, d, split);
+    }
+
+    private static void restore(int i, int j, String s, int[][] d, int[][] split) {
+        if (i > j) {
+            return;
+        }
+
+        if (i == j) {
+            if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+                System.out.print("()");
+            } else {
+                System.out.print("[]");
+            }
+            return;
+        }
+
+        if (d[i][j] == 0) {
+            System.out.print(s.substring(i, j + 1));
+            return;
+        }
+
+        if (split[i][j] == -1) {
+            System.out.print(s.charAt(i));
+            restore(i + 1, j - 1, s, d, split);
+            System.out.print(s.charAt(j));
+            return;
+        }
+
+        int k = split[i][j];
+        restore(i, k, s, d, split);
+        restore(k + 1, j, s, d, split);
+    }
+}
