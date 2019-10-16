8d074bfc69845547576251fd09b24ce9b4f38059
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-10-17 00:21:27 +0300

    done fifth task

---
+++ src/main/java/ru/mail/polis/ads/part4/bogdanmendli/FifthTask.java
@@ -0,0 +1,71 @@
+package ru.mail.polis.ads.part4.bogdanmendli;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FifthTask {
+
+    private static int n;
+    private static long maxWeight;
+    private static long[] wagonsWeight;
+
+    private static void solve() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = in.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        maxWeight = Long.parseLong(input[1]);
+        wagonsWeight = new long[n];
+        input = in.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            wagonsWeight[i] = Long.parseLong(input[i]);
+        }
+        mergeSort(0, n - 1);
+        System.out.print("Yes");
+    }
+
+    private static void mergeSort(int start, int end) {
+        if (end <= start) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        mergeSort(start, mid);
+        mergeSort(mid + 1, end);
+
+        final int n = end - start;
+        long[] temp = new long[n + 1];
+        System.arraycopy(wagonsWeight, start, temp, 0, n + 1);
+
+        int left = 0;
+        int right = n / 2 + 1;
+
+        for (int i = start; i <= end; i++) {
+            if (left > n / 2) {
+                wagonsWeight[i] = temp[right++];
+            } else if (right > n) {
+                if (temp[right - 1] + temp[left] > maxWeight) {
+                    System.out.println("No");
+                    System.exit(0);
+                }
+                wagonsWeight[i] = temp[left++];
+            } else if (temp[right] < temp[left]) {
+                if (temp[right] + temp[left] > maxWeight) {
+                    System.out.println("No");
+                    System.exit(0);
+                }
+                wagonsWeight[i] = temp[right++];
+            } else {
+                wagonsWeight[i] = temp[left++];
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            solve();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
