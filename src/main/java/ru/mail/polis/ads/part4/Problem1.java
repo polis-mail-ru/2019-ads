7af27b3dfd8af557b46144c54baf45550b9f6f96
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-10-17 18:40:52 +0300

    Problem 1

---
+++ src/main/java/ru/mail/polis/ads/part4/Problem1.java
@@ -0,0 +1,47 @@
+package ru.mail.polis.ads.part4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem1 {
+
+    private static long[] arr;
+
+    private static void solve() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        arr = new long[n];
+        String[] tokens = reader.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(tokens[i]);
+        }
+
+        if (checkForHeap(n)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    private static boolean checkForHeap(int n) {
+        for (int i = 1; 2 * i <= n; i++) {
+            if (arr[i - 1] > arr[2 * i - 1]) {
+                return false;
+            }
+            if (2 * i + 1 <= n && arr[i - 1] > arr[2 * i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        try {
+            solve();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
