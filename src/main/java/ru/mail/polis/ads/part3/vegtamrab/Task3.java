f2847fac317ba96bbb3755958ba4c79cf7ae7064
vegtamr <barekov.am.v@gmail.com>
2019-10-18 10:10:08 +0300

    Part 3 Task 3

---
+++ src/main/java/ru/mail/polis/ads/part3/vegtamrab/Task3.java
@@ -0,0 +1,73 @@
+package ru.mail.polis.ads.part3.vegtamrab;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/**
+ * Problem solution template.
+ */
+public final class Task2 {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        Integer[] array = new Integer[n];
+
+        for (int i = 0; i < n; ++i) {
+            array[i] = in.nextInt();
+        } 
+
+        int numberOfSwaps = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j < n - i; ++j) {
+                if (array[j - 1] > array[j]) {
+                    int tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                    ++numberOfSwaps;
+                }
+            }
+        }
+
+        out.print(numberOfSwaps);
+    }
+    
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        } catch (Exception ex) {
+            System.out.println();
+        }
+    }
+}
+
