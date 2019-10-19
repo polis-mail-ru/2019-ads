68de544916fcf8ad07e6e504f405fa4c747d1263
Stas Milke <svmilke@yandex.ru>
2019-10-19 17:08:59 +0300

    task1457

--- src/main/java/ru/mail/polis/ads/part4/stasmilke/Task1457.java
+++ src/main/java/ru/mail/polis/ads/part4/stasmilke/Task1457.java
@@ -8,7 +8,58 @@ public class Task1457 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        long m = in.nextLong();
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextLong();
+        }
+        boolean result = isCorrect(array, 0, n, m);
+        out.println(result ? "Yes" : "No");
+    }
 
+    private static boolean isCorrect(long[] array, int left, int right, long m) {
+        if (left + 1 >= right) {
+            return true;
+        }
+        int mid = (left + right) / 2;
+        return isCorrect(array, left, mid, m) &&
+                isCorrect(array, mid, right, m) &&
+                isCorrectSplit(array, left, mid, right, m);
+    }
+
+    private static boolean isCorrectSplit(long[] array, int left, int mid, int right, long m) {
+        int iterLeft = 0;
+        int iterMid = 0;
+        boolean isCorrect = true;
+        long[] result = new long[right - left];
+        while ((left + iterLeft < mid) && (mid + iterMid < right)) {
+            if (array[left + iterLeft] < array[mid + iterMid]) {
+                result[iterLeft + iterMid] = array[left + iterLeft];
+                iterLeft++;
+            } else {
+                if (array[left + iterLeft] + array[mid + iterMid] > m) {
+                    isCorrect = false;
+                }
+                result[iterLeft + iterMid] = array[mid + iterMid];
+                iterMid++;
+            }
+        }
+        while (left + iterLeft < mid) {
+            if (array[left + iterLeft] + array[mid + iterMid - 1] > m) {
+                isCorrect = false;
+            }
+            result[iterLeft + iterMid] = array[left + iterLeft];
+            iterLeft++;
+        }
+        while (mid + iterMid < right) {
+            result[iterLeft + iterMid] = array[mid + iterMid];
+            iterMid++;
+        }
+        for (int i = 0; i < iterLeft + iterMid; i++) {
+            array[left + i] = result[i];
+        }
+        return isCorrect;
     }
 
     private static class FastScanner {
@@ -33,12 +84,18 @@ public class Task1457 {
         int nextInt() {
             return Integer.parseInt(next());
         }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
     }
 
     public static void main(final String[] arg) {
         final FastScanner in = new FastScanner(System.in);
         try (PrintWriter out = new PrintWriter(System.out)) {
             solve(in, out);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
