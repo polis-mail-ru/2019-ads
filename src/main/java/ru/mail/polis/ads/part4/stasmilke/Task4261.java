94f9c43b6dea5c290ab49883e361efb46b03309b
Stas Milke <svmilke@yandex.ru>
2019-10-19 15:27:04 +0300

    task4261

--- src/main/java/ru/mail/polis/ads/part4/stasmilke/Task4261.java
+++ src/main/java/ru/mail/polis/ads/part4/stasmilke/Task4261.java
@@ -9,7 +9,50 @@ public class Task4261 {
     }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        int result = countInv(array, 0, n);
+        out.println(result);
+    }
 
+    private static int countInv(int[] array, int left, int right) {
+        if (left + 1 >= right) {
+            return 0;
+        }
+        int mid = (left + right) / 2;
+        return countInv(array, left, mid) + countInv(array, mid, right) + countSplitInv(array, left, mid, right);
+    }
+
+    private static int countSplitInv(int[] array, int left, int mid, int right) {
+        int iterLeft = 0;
+        int iterMid = 0;
+        int counter = 0;
+        int[] result = new int[right - left];
+        while ((left + iterLeft < mid) && (mid + iterMid < right)) {
+            if (array[left + iterLeft] < array[mid + iterMid]) {
+                result[iterLeft + iterMid] = array[left + iterLeft];
+                iterLeft++;
+            } else {
+                result[iterLeft + iterMid] = array[mid + iterMid];
+                iterMid++;
+                counter += mid - left - iterLeft;
+            }
+        }
+        while (left + iterLeft < mid) {
+            result[iterLeft + iterMid] = array[left + iterLeft];
+            iterLeft++;
+        }
+        while (mid + iterMid < right) {
+            result[iterLeft + iterMid] = array[mid + iterMid];
+            iterMid++;
+        }
+        for (int i = 0; i < iterLeft + iterMid; i++) {
+            array[left + i] = result[i];
+        }
+        return counter;
     }
 
     private static class FastScanner {
