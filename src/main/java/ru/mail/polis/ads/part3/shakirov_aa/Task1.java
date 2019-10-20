a8f8ee7491d4fe44d0e2d20a6e2f4fc33ef7f696
Almaz <almick17933@gmail.com>
2019-10-20 20:16:56 +0300

    changed task1, mergeSort implemented

--- src/main/java/ru/mail/polis/ads/part3/shakirov_aa/Task1.java
+++ src/main/java/ru/mail/polis/ads/part3/shakirov_aa/Task1.java
@@ -1,7 +1,6 @@
 package ru.mail.polis.ads.part3.shakirov_aa;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Task1 {
@@ -13,13 +12,65 @@ public class Task1 {
             arr[i] = fs.nextInt();
         }
 
-        Arrays.sort(arr);
+        mergeSort(arr, 0, N - 1);
         PrintWriter out = new PrintWriter(System.out);
 
         for (int i = 0; i < N; i++) {
             out.print(arr[i] + " ");
         }
         out.close();
+    }
+
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+
+            merge(arr, left, middle, right);
+        }
+    }
+
+    static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 =  right - middle;
+
+        int[] l = new int[n1];
+        int[] r = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            l[i] = arr[left + i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            r[j] = arr[middle + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (l[i] <= r[j]) {
+                arr[k] = l[i];
+                i++;
+            } else {
+                arr[k] = r[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = l[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = r[j];
+            j++;
+            k++;
+        }
     }
 
     private static class FastScanner {
