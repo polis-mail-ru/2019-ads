f6a04e78ce31f4122edd8538f95ade7926723cf7
Saprykin Vitaly <sapryk1n.vitaliy@yandex.ru>
2019-10-20 20:40:23 +0300

    Part 4. First three tasks completed. Start for inversions

---
+++ src/main/java/ru/mail/polis/ads/part4/kokobox7/Inversions.java
@@ -0,0 +1,42 @@
+package ru.mail.polis.ads.part4.kokobox7;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Inversions {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+       int n = in.nextInt();
+       int[] array  = new int[n];
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
