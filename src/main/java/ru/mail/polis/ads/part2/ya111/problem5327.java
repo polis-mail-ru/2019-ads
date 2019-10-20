99b07318bee753ac98b481aa4dfd3cf08b849275
ya111 <ppanichkin@ya.ru>
2019-10-20 19:36:39 +0300

    Update problem5327.java

--- src/main/java/ru/mail/polis/ads/part2/ya111/problem5327.java
+++ src/main/java/ru/mail/polis/ads/part2/ya111/problem5327.java
@@ -5,7 +5,7 @@ public final class problem5327 {
     private problem5327(){
     }
 
-    public static void main(final String[] argc) {
+    public static void main(String[] argc) {
         Scanner sc = new Scanner(System.in);
         String string = sc.nextLine();
         Stack<Integer> stack = new Stack<>();
@@ -26,4 +26,4 @@ public final class problem5327 {
         else
             System.out.println("NO");
     }
-} 
+}
