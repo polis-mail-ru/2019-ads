584d6fc7d83d54931f61a118473ce95395b27571
ya111 <ppanichkin@ya.ru>
2019-10-20 19:40:11 +0300

    Update problem6124.java

---
+++ src/main/java/ru/mail/polis/ads/part2/ya111/problem6124.java
@@ -0,0 +1,58 @@
+package ru.mail.polis.ads.part2.ya111;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public final class problem6124 {
+    private problem6124(){
+    }
+
+    public static void main(final String[] argc) throws IOException {
+        LinkedList<String> myArrayDeque = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean a = true;
+        while (a)
+        {
+            String string = br.readLine();
+            switch (string)
+            {
+                case ("pop"):
+                    if (myArrayDeque.peekLast() != null)
+                        System.out.println(myArrayDeque.pollLast());
+                    else
+                        System.out.println("error");
+                    break;
+
+                case ("back"):
+                    if (myArrayDeque.peekLast() != null)
+                        System.out.println(myArrayDeque.peekLast());
+                    else
+                        System.out.println("error");
+                    break;
+
+                case ("size"):
+                    System.out.println(myArrayDeque.size());
+                    break;
+
+                case ("clear"):
+                    myArrayDeque.clear();
+                    System.out.println("ok");
+                    break;
+
+                case ("exit"):
+                    System.out.println("bye");
+                    a=false;
+                    break;
+
+                default:
+                    System.out.println("ok");
+                    String[] parts = string.split(" ");
+                    myArrayDeque.add(parts[1]);
+                    break;
+            }
+        }
+    }
+} 
