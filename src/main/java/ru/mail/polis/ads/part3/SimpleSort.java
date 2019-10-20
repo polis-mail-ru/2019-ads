aca23a64c88108d5d1e552fae81c6d0a17b8d417
Saprykin Vitaly <sapryk1n.vitaliy@yandex.ru>
2019-10-20 10:51:22 +0300

    First two tasks completed.

---
+++ src/main/java/ru/mail/polis/ads/part3/SimpleSort.java
@@ -0,0 +1,50 @@
+package ru.mail.polis.ads.part3;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SimpleSort {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        Arrays.sort(array);
+        for (int i : array) {
+            out.print(i + " ");
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
---
+++ src/main/java/ru/mail/polis/ads/part3/TrickySort.java
@@ -0,0 +1,61 @@
+package ru.mail.polis.ads.part3;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class TrickySort {
+
+     static class TrickyComparator implements java.util.Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return 0;
+        }
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        Integer[] array = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+
+        Arrays.sort(array, new TrickyComparator());
+        for (int i : array) {
+            out.print(i + " ");
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+
+}
