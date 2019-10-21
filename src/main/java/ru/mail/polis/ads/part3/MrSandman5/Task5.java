14fda20ef9c3ab12d3594f337849e4eb5c9e6dfd
Simon Safonov <mrsandman138@gmail.com>
2019-10-21 12:47:51 +0300

    Task1 and Task5 fixed

--- src/main/java/ru/mail/polis/ads/part3/MrSandman5/Task5.java
+++ src/main/java/ru/mail/polis/ads/part3/MrSandman5/Task5.java
@@ -1,10 +1,9 @@
 package part3;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// https://www.e-olymp.com/ru/submissions/5897479
+// https://www.e-olymp.com/ru/submissions/5910781
 
 public class Task5 {
 
@@ -30,8 +29,65 @@ public class Task5 {
         for (int i = 0; i < n; i++) {
             numbers[i] = new Key(in.nextInt(), in.nextInt(), i);
         }
-        Arrays.sort(numbers, 0, n);
+        sort(numbers, 0, n - 1);
         for(int i = 0; i < n; i++) out.println(numbers[i].i + " " + numbers[i].j);
+    }
+
+    private static void merge(Key[] arr, int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Key[] left = new Key [n1];
+        Key[] right = new Key [n2];
+
+        System.arraycopy(arr, l, left, 0, n1);
+        for (int j = 0; j < n2; j++)
+            right[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (left[i].compareTo(right[j]) <= 0)
+            {
+                arr[k] = left[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+            while (j < n2)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+        }
+    }
+
+    private static void sort(Key[] arr, int l, int r)
+    {
+        if (l < r)
+        {
+            int m = (l + r) / 2;
+
+            sort(arr, l, m);
+            sort(arr , m + 1, r);
+            merge(arr, l, m, r);
+        }
     }
 
     private static class FastScanner {
