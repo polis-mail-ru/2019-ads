9d3fac66bfdecdddfedbfd693d9ade5bbdb563f9
nik27090 <nik27090@mail.ru>
2019-10-22 20:00:25 +0300

    add Diplomas

---
+++ src/main/java/ru/mail/polis/ads/part5/nik27090/Diplomas.java
@@ -0,0 +1,26 @@
+package ru.mail.polis.ads.part5.nik27090;
+
+import java.util.Scanner;
+
+public class Diplomas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long w=sc.nextInt();
+        long h=sc.nextInt();
+        long  n=sc.nextInt();
+        long left = Math.max(w,h);
+        long right= n*left;
+        long m;
+        long v;
+        while (left<right){
+            m=(left+right)/2;
+            v = (m/w)*(m/h);
+            if (v>=n){
+                right=m;
+            } else {
+                left=m+1;
+            }
+        }
+        System.out.println(right);
+    }
+}
