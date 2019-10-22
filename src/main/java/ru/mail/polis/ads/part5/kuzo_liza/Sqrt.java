6d1e84a9df3ce7c97f66064a59fb035bdefc4d2c
kuzo-liza <kuzo-liza@yandex.ru>
2019-10-22 19:48:38 +0300

    https://www.e-olymp.com/ru/submissions/5924240

---
+++ src/main/java/ru/mail/polis/ads/part5/kuzo_liza/Sqrt.java
@@ -0,0 +1,74 @@
+package ru.mail.polis.ads.part5.kuzo_liza;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Sqrt {
+    private Sqrt() {
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        double c = in.nextDouble();
+
+        out.println(
+                String.format("%.6f", findResult(0, c, c)));
+    }
+
+
+    private static double findResult(double left, double right, double c) {
+        double median = (left + right) / 2;
+
+        while (Math.abs(f(median) - c) > 1e-6) {
+            if (f(median) > c) {
+                right = median;
+            } else {
+                left = median;
+            }
+            median = (left + right) / 2;
+        }
+        return median;
+    }
+
+    private static double f(double x) {
+        return x * x + Math.sqrt(x);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
