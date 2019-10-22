9b795c2ec51ffdbb1bf7774c8f6ecc5b0abdb000
Eretic431 <abdullapower@outlook.com>
2019-10-22 19:15:30 +0300

    Task3

---
+++ src/main/java/ru/mail/polis/ads/part4/Eretic431/Task3.java
@@ -0,0 +1,37 @@
+package main.java.ru.mail.polis.ads.part4.Eretic431;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
