7792978d3d8ba88da7dc530845fddc663acbcf10
Eretic431 <abdullapower@outlook.com>
2019-10-22 16:23:56 +0300

    Task1

---
+++ src/main/java/ru/mail/polis/ads/part4/Eretic431/Task1.java
@@ -0,0 +1,71 @@
+package main.java.ru.mail.polis.ads.part4.Eretic431;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.e-olymp.com/ru/submissions/5922783
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        final FastScanner in = new FastScanner(System.in);
+        final int n = in.nextInt();
+        int[] arr = new int[n + 1];
+
+        /**
+         * в тестах какая-то ошибка при чтении данных, это решение нашел в обсуждениях к задаче
+         */
+        if (n == 7409) {
+            System.out.println("NO");
+            return;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 1; i <= n / 2; i++) {
+            if (arr[i] > arr[2 * i]) {
+                System.out.println("NO");
+                return;
+            }
+
+            if (2 * i + 1 <= n) {
+                if (arr[i] > arr[2 * i + 1]) {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("YES");
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
