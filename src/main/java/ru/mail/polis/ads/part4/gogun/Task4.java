3c922f45d8542d2601af8317da07c4f2b9921055
gogun <gusarov2000@gmail.com>
2019-10-22 03:08:20 +0300

    add task 4

---
+++ src/main/java/ru/mail/polis/ads/part4/gogun/Task4.java
@@ -0,0 +1,79 @@
+package ru.mail.polis.ads.part4.gogun;
+
+import java.io.*;
+import java.util.*;
+
+public class Task4 {
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final BufferedInputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static int merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, count = 0;
+        while (i < left.length || j < right.length) {
+            if (i == left.length) {
+                arr[i+j] = right[j];
+                j++;
+            } else if (j == right.length) {
+                arr[i+j] = left[i];
+                i++;
+            } else if (left[i] <= right[j]) {
+                arr[i+j] = left[i];
+                i++;
+            } else {
+                arr[i+j] = right[j];
+                count += left.length-i;
+                j++;
+            }
+        }
+        return count;
+    }
+
+    private static int invCount(int[] arr) {
+        if (arr.length < 2)
+            return 0;
+
+        int m = (arr.length + 1) / 2;
+        int left[] = Arrays.copyOfRange(arr, 0, m);
+        int right[] = Arrays.copyOfRange(arr, m, arr.length);
+
+        return invCount(left) + invCount(right) + merge(arr, left, right);
+    }
+
+    private static void solve(FastScanner input, PrintWriter output) {
+        int n = input.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; ++i) {
+            array[i] = input.nextInt();
+        }
+
+        output.println(invCount(array));
+    }
+
+    public static void main(String[] args) {
+        FastScanner in = new FastScanner(new BufferedInputStream(System.in));
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out))) {
+            solve(in, out);
+        }
+    }
+}
