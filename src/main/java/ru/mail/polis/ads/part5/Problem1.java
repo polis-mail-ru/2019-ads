531b08f5e45b7f12ad6db69be563d37d3ca183f1
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-10-22 19:08:33 +0300

    Problem 1

---
+++ src/main/java/ru/mail/polis/ads/part5/Problem1.java
@@ -0,0 +1,29 @@
+package ru.mail.polis.ads.part5;
+
+import java.util.Scanner;
+
+public class Problem1 {
+
+    private static void solve() {
+        double c = new Scanner(System.in).nextDouble();
+        double left = 0;
+        double right = c;
+        while (true) {
+            double x = (left + right) / 2;
+            double y = x*x + Math.sqrt(x);
+            if (Math.abs(y - c) < 0.0000001) {
+                System.out.println(x);
+                break;
+            }
+            if (y < c) {
+                left = x;
+            } else {
+                right = x;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
