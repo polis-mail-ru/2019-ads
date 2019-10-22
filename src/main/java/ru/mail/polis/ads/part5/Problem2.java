95f0b904c6cfc901f1bb0dcd4cd60a5da0497d24
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-10-22 20:18:22 +0300

    Problem 2

---
+++ src/main/java/ru/mail/polis/ads/part5/Problem2.java
@@ -0,0 +1,32 @@
+package ru.mail.polis.ads.part5;
+
+import java.util.Scanner;
+
+public class Problem2 {
+
+    private static void solve() {
+        Scanner sc = new Scanner(System.in);
+        long w = sc.nextInt();
+        long h = sc.nextInt();
+        long n = sc.nextInt();
+
+        long left = Math.max(w, h);
+        long right = n * Math.max(w, h);
+
+        do {
+            long m = (left + right) / 2;
+            long v = (m / w) * (m / h);
+            if (v >= n) {
+                right = m;
+            } else {
+                left = m + 1;
+            }
+        } while (left < right);
+
+        System.out.println(left);
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
