8329cda91121d3506e9c9989f7fc7a2e414e7f6d
Eretic431 <abdullapower@outlook.com>
2019-10-22 20:31:33 +0300

    Task2

---
+++ src/main/java/ru/mail/polis/ads/part5/Eretic431/Task2.java
@@ -0,0 +1,32 @@
+package main.java.ru.mail.polis.ads.part5.Eretic431;
+
+import java.util.Scanner;
+
+/**
+ * https://www.e-olymp.com/ru/submissions/5924652
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+        final Scanner in = new Scanner(System.in);
+        final int w = in.nextInt();
+        final int h = in.nextInt();
+        final int n = in.nextInt();
+
+        long start = Math.max(w, h);
+        long end = n * Math.max(w, h);
+        long m = 0;
+
+        while (start < end) {
+            m = (start + end) / 2;
+            long v = (m / w) * (m / h);
+            if (n <= v) {
+                end = m;
+            } else {
+                start = m + 1;
+            }
+        }
+
+        System.out.println(start);
+    }
+}
