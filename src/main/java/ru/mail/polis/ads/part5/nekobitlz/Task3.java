0ab36de72ad38d8ef3cd0b5ef17d881273edca0a
Nekobitlz <jokers.bit@gmail.com>
2019-10-24 03:04:00 +0300

    Solve third task

---
+++ src/main/java/ru/mail/polis/ads/part5/nekobitlz/Task3.java
@@ -0,0 +1,63 @@
+package ru.mail.polis.ads.part5.nekobitlz;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Task3 {
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] a = new int[n];
+        int[] d = new int[n];
+        int ans = 1;
+        d[0] = 1;
+
+        for (int i = 0; i < n; i++) a[i] = in.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (a[j] != 0 && a[i] % a[j] == 0 && d[j] > max) {
+                    max = d[j];
+                }
+            }
+
+            d[i] = max + 1;
+            if (d[i] > ans) ans = d[i];
+        }
+
+        out.println(ans);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
