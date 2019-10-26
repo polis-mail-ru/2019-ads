1f7dbec6acb966da3d0ac4e85d3907537b345bda
jhoysbou <neeroll21Wgmail.com>
2019-10-26 20:12:08 +0300

    Third problem resolved. (Still doesnt work)

--- src/main/java/ru/mail/polis/ads/part4/jhoysbou/ThirdExercise.java
+++ src/main/java/ru/mail/polis/ads/part4/jhoysbou/ThirdExercise.java
@@ -1,16 +1,14 @@
 package ru.mail.polis.ads.part4.jhoysbou;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 // Submission here https://www.e-olymp.com/ru/submissions/5907291
 
 public class ThirdExercise {
 
-    public static class MyComparator implements Comparator<Integer> {
-
+    private static class MyComparator implements Comparator<Integer> {
         @Override
         public int compare(Integer firstNumber, Integer secondNumber) {
             return firstNumber > secondNumber ? -1 : firstNumber < secondNumber ? 1 : 0;
@@ -19,11 +17,11 @@ public class ThirdExercise {
     }
 
     public static void main(String[] args) throws IOException {
+        final int maxSize = 1000000000;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
-        PriorityQueue<Integer> leftHeap = new PriorityQueue<>(new MyComparator());
-        PriorityQueue<Integer> rightHeap = new PriorityQueue<>();
-
+        Heap leftHeap = new Heap(maxSize/2 + 2);
+        Heap rightHeap = new Heap(maxSize/2 + 2, new MyComparator());
 
         Integer nextNumber = null;
         try {
@@ -43,12 +41,12 @@ public class ThirdExercise {
 
             if (median != Integer.MAX_VALUE && !isEmpty) {
                 if (nextNumber >= median) {
-                    rightHeap.add(nextNumber);
-                    leftHeap.add(median);
+                    rightHeap.insert(nextNumber);
+                    leftHeap.insert(median);
                 }
                 else if (nextNumber < median) {
-                    leftHeap.add(nextNumber);
-                    rightHeap.add(median);
+                    leftHeap.insert(nextNumber);
+                    rightHeap.insert(median);
                 }
                 out.println((leftHeap.peek() + rightHeap.peek()) / 2);
                 median = Integer.MAX_VALUE;
@@ -56,11 +54,11 @@ public class ThirdExercise {
             else if (!isEmpty) {
                 median = (rightHeap.peek() + leftHeap.peek()) / 2;
                 if (nextNumber >= median) {
-                    rightHeap.add(nextNumber);
+                    rightHeap.insert(nextNumber);
                     median = rightHeap.poll();
                 }
                 else {
-                    leftHeap.add(nextNumber);
+                    leftHeap.insert(nextNumber);
                     median = leftHeap.poll();
                 }
                 out.println(median);
@@ -76,6 +74,101 @@ public class ThirdExercise {
         out.flush();
         out.close();
         reader.close();
+    }
+
+    private static class Heap {
+        private int[] Heap;
+        private int size;
+        private int capacity;
+        private Comparator<Integer> c = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 > o2 ? 1 : o1 < o2 ? -1 : 0;
+            }
+        };
+
+        Heap(int capacity) {
+            this.capacity = capacity;
+            this.size = 0;
+            Heap = new int[capacity + 1];
+            Arrays.fill(Heap, Integer.MAX_VALUE);
+
+        }
+
+        Heap(int capacity, Comparator<Integer> c) {
+            this.c = c;
+            this.capacity = capacity;
+            this.size = 0;
+            Heap = new int[capacity + 1];
+        }
+
+        int leftChild(int parent) {
+            return 2*parent;
+        }
+
+        int rightChild(int parent) {
+            return 2*parent + 1;
+        }
+
+        private boolean isLeaf(int pos)
+        {
+            if (pos >= (size / 2) && pos <= size) {
+                return true;
+            }
+            return false;
+        }
+
+        private void swap(int fpos, int spos)
+        {
+            int tmp;
+            tmp = Heap[fpos];
+            Heap[fpos] = Heap[spos];
+            Heap[spos] = tmp;
+        }
+
+        void heapify(int pos) {
+            if (isLeaf(pos))
+                return;
+
+            if (c.compare( Heap[pos], Heap[leftChild(pos)]) == -1 ||
+                    c.compare(Heap[pos], Heap[rightChild(pos)]) == -1) {
+
+                if (c.compare(Heap[leftChild(pos)], Heap[rightChild(pos)]) == 1) {
+                    swap(pos, leftChild(pos));
+                    heapify(leftChild(pos));
+                }
+                else {
+                    swap(pos, rightChild(pos));
+                    heapify(rightChild(pos));
+                }
+            }
+        }
+
+        private int parent(int pos)
+        {
+            return pos / 2;
+        }
+
+        void insert(int element) {
+            Heap[++size] = element;
+
+            int current = size;
+            while (c.compare(Heap[current], Heap[parent(current)]) == 1) {
+                swap(current, parent(current));
+                current = parent(current);
+            }
+        }
+        int peek() {
+            return Heap[1];
+        }
+
+        int poll()
+        {
+            int popped = Heap[1];
+            Heap[1] = Heap[size--];
+            heapify(1);
+            return popped;
+        }
     }
 
 }
