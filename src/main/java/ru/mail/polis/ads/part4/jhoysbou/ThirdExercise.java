bc117baa81536a29c14d467f0fdeffa8b59fe253
jhoysbou <neeroll21Wgmail.com>
2019-10-28 19:59:57 +0300

    Some changes.

--- src/main/java/ru/mail/polis/ads/part4/jhoysbou/ThirdExercise.java
+++ src/main/java/ru/mail/polis/ads/part4/jhoysbou/ThirdExercise.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
 
-// Submission here https://www.e-olymp.com/ru/submissions/5964946
+// Submission here https://www.e-olymp.com/ru/submissions/5965364
 
 public class ThirdExercise {
 
