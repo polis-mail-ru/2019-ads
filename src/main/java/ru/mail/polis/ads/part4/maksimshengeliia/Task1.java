d065fcd14c0911f85da178008678009515320730
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-10-28 13:05:06 +0300

    Solved Task 1, 2

--- src/main/java/ru/mail/polis/ads/part4/maksimshengeliia/Task1.java
+++ src/main/java/ru/mail/polis/ads/part4/maksimshengeliia/Task1.java
@@ -1,52 +1,30 @@
 package ru.mail.polis.ads.part4.maksimshengeliia;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-/**
- * Problem solution template.
- */
-public final class Task1 {
-    private Task1() {
-        // Should not be instantiated
-    }
-
-    private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
-    }
-
-    private static class FastScanner {
-        private final BufferedReader reader;
-        private StringTokenizer tokenizer;
-
-        FastScanner(final InputStream in) {
-            reader = new BufferedReader(new InputStreamReader(in));
-        }
-
-        String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return tokenizer.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-    }
-
+/*
+*   https://www.e-olymp.com/ru/submissions/5963786
+* */
+public class Task1 {
     public static void main(final String[] arg) {
-        final FastScanner in = new FastScanner(System.in);
-        try (PrintWriter out = new PrintWriter(System.out)) {
-            solve(in, out);
+        int i, n;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        long[] m = new long[n + 1];
+        for (i = 1; i <= n; i++) {
+            m[i] = in.nextLong();
+        }
+
+
+        for (i = 1; i <= n / 2; i++) {
+            if (2 * i <= n && m[i] > m[2 * i]) break;
+            if (2 * i + 1 <= n && m[i] > m[2 * i + 1]) break;
+        }
+
+        if (i <= n / 2) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
         }
     }
 }
--- src/main/java/ru/mail/polis/ads/part4/maksimshengeliia/Task2.java
+++ src/main/java/ru/mail/polis/ads/part4/maksimshengeliia/Task2.java
@@ -1,22 +1,81 @@
 package ru.mail.polis.ads.part4.maksimshengeliia;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 /**
  * Problem solution template.
  */
-public final class Task2 {
+public class Task2 {
     private Task2() {
         // Should not be instantiated
     }
+    private static class Heap {
 
+        int[] array;
+        int size;
+        Heap(int n) {
+            array = new int[n];
+            size = 0;
+        }
+
+        void insert(int v) {
+            size++;
+            array[size - 1] = v;
+            swim(size - 1);
+        }
+
+        int extract() {
+            int max = array[0];
+            array[0] = array[size - 1];
+            size--;
+            sink();
+            return max;
+        }
+
+        void sink() {
+            int index = 0;
+            int left, right, j;
+            while ((left = 2 * index + 1) < size) {
+                right = left + 1;
+                if (array[right] > array[left]) {
+                    j = right;
+                } else {
+                    j = left;
+                }
+                if (array[index] >= array[j]) {
+                    break;
+                }
+                swap(index, j);
+                index = j;
+            }
+        }
+
+        void swim(int index) {
+            int parentIndex = (index - 1) / 2;
+            while (array[index] > array[parentIndex]) {
+                swap(index, parentIndex);
+                index = parentIndex;
+            }
+        }
+
+        void swap(final int a, final int b) {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        Heap heap = new Heap(n);
+        for (int i = 0; i < n; i++) {
+           int command = in.nextInt();
+           if (command == 0) {
+               heap.insert(in.nextInt());
+           } else {
+                out.println(heap.extract());
+           }
+        }
     }
 
     private static class FastScanner {
