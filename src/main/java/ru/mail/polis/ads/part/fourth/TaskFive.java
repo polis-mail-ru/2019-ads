e38c6b2e40f83dcbda49ff9d0001bea0748cc5e2
v.ivlev <nevidd@yandex.ru>
2019-10-28 23:13:17 +0300

    Task 5
    See: https://www.e-olymp.com/ru/submissions/5971659

--- src/main/java/ru/mail/polis/ads/part/fourth/TaskFive.java
+++ src/main/java/ru/mail/polis/ads/part/fourth/TaskFive.java
@@ -1,12 +1,66 @@
 package ru.mail.polis.ads.part.fourth;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * @author v.ivlev
+ * https://www.e-olymp.com/ru/submissions/5971659
+ */
 public class TaskFive {
 
-    private static void solve(FastScanner in, PrintWriter out) {
+    private static boolean flag;
 
+    private static void solve(FastScanner in, PrintWriter out) {
+        int[] array = new int[in.nextInt()];
+        int m = in.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+        }
+        mergeSort(array, array.length, m);
+        if (!flag) {
+            out.println("Yes");
+        } else {
+            out.println("No");
+        }
+    }
+
+    private static int[] mergeSort(int[] array, int size, int m) {
+        if (size > 1) {
+            int mid = size / 2;
+            int[] arrayLeft = Arrays.copyOfRange(array, 0, mid);
+            int[] arrayRight = Arrays.copyOfRange(array, mid, size);
+            arrayLeft = mergeSort(arrayLeft, mid, m);
+            arrayRight = mergeSort(arrayRight, size - mid, m);
+            array = merge(arrayLeft, arrayRight, mid, size - mid, m);
+        }
+        return array;
+    }
+
+    private static int[] merge(int arrayLeft[], int arrayRight[], int l, int r, int m) {
+        int array[] = new int[l + r];
+        int i = 0;
+        int j = 0;
+        int n = 0;
+        while (j < l && n < r) {
+            if (arrayLeft[j] < arrayRight[n]) {
+                array[i++] = arrayLeft[j++];
+            } else {
+                array[i++] = arrayRight[n++];
+                if (m < arrayLeft[l - 1] + arrayRight[n - 1]) {
+                    flag = true;
+                    return array;
+                }
+            }
+        }
+        while (j < l) {
+            array[i++] = arrayLeft[j++];
+        }
+        while (n < r) {
+            array[i++] = arrayRight[n++];
+        }
+        return array;
     }
 
     public static void main(final String[] arg) {
