3b85caa905679d3ebd4315cff7e2772e3af5d1da
v.ivlev <v.ivlev@geoscan.aero>
2019-10-29 09:41:00 +0300

    Task 7
    See: https://www.e-olymp.com/ru/submissions/5973023

--- src/main/java/ru/mail/polis/ads/part/fourth/TaskSeven.java
+++ src/main/java/ru/mail/polis/ads/part/fourth/TaskSeven.java
@@ -7,10 +7,43 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
+/**
+ * @author v.ivlev
+ * https://www.e-olymp.com/ru/submissions/5973023
+ */
 public class TaskSeven {
 
     private static void solve(FastScanner in, PrintWriter out) {
-
+        int[] array = new int[in.nextInt()];
+        int n = in.nextInt();
+        int left = 0;
+        int j = 0;
+        int right = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+        }
+        right = array[array.length - 1] - array[0];
+        if (n == 2) {
+            out.println(right);
+        } else {
+            while (left < right) {
+                int cowResidue = n - 1;
+                int m = (left + right) / 2;
+                for (int i = 1; i < array.length; i++) {
+                    if (m <= array[i] - array[j]) {
+                        j = i;
+                        cowResidue--;
+                    }
+                }
+                if (cowResidue > 0) {
+                    right = m;
+                } else {
+                    left = m + 1;
+                }
+                j = 0;
+            }
+            out.println(left - 1);
+        }
     }
 
     public static void main(final String[] arg) {
