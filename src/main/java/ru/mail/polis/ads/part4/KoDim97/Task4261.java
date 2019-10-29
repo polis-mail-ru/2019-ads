79f6cdc03ee4a96e8c47971896db65b15f27dd1d
KoDim97 <kodim97@yandex.ru>
2019-10-29 12:46:59 +0300

    Add 4261 task

--- src/main/java/ru/mail/polis/ads/part4/KoDim97/Task4261.java
+++ src/main/java/ru/mail/polis/ads/part4/KoDim97/Task4261.java
@@ -14,23 +14,78 @@ public final class Task4261 {
     private Task4261() {
         // Should not be instantiated
     }
-    static int CountSplitInv(int[] arr, int i, int j, int mid){
+    static int Merge(int[] arr, int l, int m, int r) {
+        int i, j, k;
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int numOfInvers = 0;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
 
-    }
-    static int CountInverse(int[] arr, int i, int j){
-        if (j - i <= 1){
-            return 0;
+        for (i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+        i = 0;
+        j = 0;
+        k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                numOfInvers += (L.length - i);
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
         }
-        int mid = (i + j) /2;
-        return CountInverse(arr, i, mid) + CountInverse(arr, mid + 1, j) + CountSplitInv(arr, i, j, mid);
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+        return numOfInvers;
     }
+
+    static int MergeSort(int [] arr, int n) {
+        int curr_size;
+        int left_start;
+        int ans = 0;
+        for (curr_size = 1; curr_size <= n-1;
+             curr_size = 2*curr_size)
+        {
+            for (left_start = 0; left_start < n-1;
+                 left_start += 2*curr_size)
+            {
+                int mid = Math.min(left_start + curr_size - 1, n-1);
+
+                int right_end = Math.min(left_start
+                        + 2*curr_size - 1, n-1);
+                ans += Merge(arr, left_start, mid, right_end);
+            }
+        }
+        return ans;
+    }
+
     private static void solve(final FastScanner in, final PrintWriter out) {
         int n = in.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-
+        out.print(MergeSort(arr, n));
     }
 
     private static class FastScanner {
