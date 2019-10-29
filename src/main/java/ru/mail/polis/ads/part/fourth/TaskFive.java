b8aa80cfe09dff7c81ea1706ce6681157dc35e3f
v.ivlev <v.ivlev@geoscan.aero>
2019-10-29 08:44:41 +0300

    Checkstyle

--- src/main/java/ru/mail/polis/ads/part/fourth/TaskFive.java
+++ src/main/java/ru/mail/polis/ads/part/fourth/TaskFive.java
@@ -1,6 +1,10 @@
 package ru.mail.polis.ads.part.fourth;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
