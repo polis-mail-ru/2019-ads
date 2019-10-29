973af234117e163ad351104f6fe3e47ab5f7828a
Dmitry <dimacaesar@gmail.com>
2019-10-29 13:56:56 +0300

    add part 4 Task2Heapify

---
+++ src/main/java/ru/mail/polis/ads/part4/DiscreetDmitriy/Task2Heapify.java
@@ -0,0 +1,59 @@
+package ru.mail.polis.ads.part4.DiscreetDmitriy;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Task2Heapify {
+    private Task2Heapify() {
+    }
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        final int n = in.nextInt();
+        PriorityQueue<Integer> digits =
+                new PriorityQueue<>(Collections.reverseOrder());
+
+        int i = 0;
+
+        while (i < n) {
+            if (in.nextInt() == 0)
+                digits.add(in.nextInt());
+            else
+                out.println(digits.poll());
+
+            i++;
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
