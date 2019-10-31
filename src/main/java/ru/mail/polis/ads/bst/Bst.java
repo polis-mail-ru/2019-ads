d5d54128757f445409b36fbf3095eb63cbaccd1b
Artem Gerasimov <39194008+art241111@users.noreply.github.com>
2019-10-31 18:00:04 +0300

    Create avl-trees

---
+++ src/main/java/ru/mail/polis/ads/bst/Bst.java
@@ -0,0 +1,33 @@
+package ru.mail.polis.ads.bst;
+
+/**
+ * Binary search tree with ordered operations support.
+ *
+ *  Create by Artem Gerasimov
+ *  Date 30.10.2019
+ *  GIT: art241111
+ */
+
+public interface Bst<Key extends Comparable<Key>, Value> {
+    Value get(Key key);
+
+    void put(Key key, Value value);
+
+    Value remove(Key key);
+
+    Key min();
+
+    Value minValue();
+
+    Key max();
+
+    Value maxValue();
+
+    Key floor(Key key);
+
+    Key ceil(Key key);
+
+    int size();
+
+    int height();
+}
---
+++ src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java
@@ -0,0 +1,112 @@
+package ru.mail.polis.ads.bst;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Basic binary search tree invariants.
+ */
+class BstBaseTest {
+
+    Bst<String, String> newBst() {
+        return new AvlBst<>();
+    }
+
+    @Test
+    void emptyBst() {
+        Bst<String, String> bst = newBst();
+        assertNull(bst.get(""));
+        assertNull(bst.get("some key"));
+        assertEquals(0, bst.size());
+        assertEquals(0, bst.height());
+    }
+
+    @Test
+    void orderedOnEmpty() {
+        Bst<String, String> bst = newBst();
+        assertNull(bst.ceil("some key"));
+        assertNull(bst.floor("some key"));
+
+        assertNull(bst.min());
+        assertNull(bst.max());
+
+        assertNull(bst.minValue());
+        assertNull(bst.maxValue());
+    }
+
+    @Test
+    void put() {
+        Bst<String, String> bst = newBst();
+        bst.put("foo", "bar");
+
+        assertEquals("bar", bst.get("foo"));
+
+        assertEquals(1, bst.size());
+        assertEquals(1, bst.height());
+    }
+
+    @Test
+    void replace() {
+        Bst<String, String> bst = newBst();
+        bst.put("foo", "bar");
+        bst.put("foo", "bee");
+
+        assertEquals("bee", bst.get("foo"));
+
+        assertEquals(1, bst.size());
+        assertEquals(1, bst.height());
+    }
+
+    @Test
+    void remove() {
+        Bst<String, String> bst = newBst();
+        bst.put("foo", "bar");
+        bst.put("foo2", "bee");
+
+        assertEquals("bar", bst.remove("foo"));
+
+        assertEquals(1, bst.size());
+        assertEquals(1, bst.height());
+    }
+
+
+    @Test
+    void floor() {
+        Bst<String, String> bst = newBst();
+        bst.put("foo1", "bee");
+        bst.put("foo6", "bee");
+        bst.put("foo3", "bee");
+        bst.put("foo7", "bee");
+        bst.put("foo2", "bee");
+        bst.put("foo4", "bee");
+        bst.put("foo9", "bee");
+
+        assertEquals("foo4", bst.floor("foo5"));
+        assertEquals("foo7", bst.floor("foo7"));
+        assertEquals("foo9", bst.floor("foo999"));
+        assertEquals("foo2", bst.floor("foo2"));
+        assertEquals("foo1", bst.floor("foo1"));
+        assertEquals("foo7", bst.floor("foo8"));
+        assertNull(bst.floor("foo"));
+    }
+
+    @Test
+    void ceil() {
+        Bst<String, String> bst = newBst();
+        bst.put("foo1", "bee");
+        bst.put("foo6", "bee");
+        bst.put("foo3", "bee");
+        bst.put("foo7", "bee");
+        bst.put("foo2", "bee");
+        bst.put("foo4", "bee");
+        bst.put("foo9", "bee");
+
+        assertEquals("foo6", bst.ceil("foo5"));
+        assertEquals("foo7", bst.ceil("foo7"));
+        assertNull(bst.ceil("foo999"));
+        assertEquals("foo2", bst.ceil("foo2"));
+        assertEquals("foo1", bst.ceil("foo1"));
+        assertEquals("foo9", bst.ceil("foo8"));
+        assertEquals("foo1", bst.ceil("foo"));
+    }
+}
