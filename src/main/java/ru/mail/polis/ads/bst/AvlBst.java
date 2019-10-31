3a55738f1024671f00f97cb9818f5009166ed102
Almaz <almick17933@gmail.com>
2019-10-31 04:57:13 +0300

    avl tree

--- src/main/java/ru/mail/polis/ads/bst/AvlBst.java
+++ src/main/java/ru/mail/polis/ads/bst/AvlBst.java
@@ -12,7 +12,14 @@ public class AvlBst<Key extends Comparable<Key>, Value>
         Node left;
         Node right;
         int height;
+
+        public Node(Key key, Value value, int height) {
+            this.key = key;
+            this.value = value;
+            this.height = height;
+        }
     }
+    private Node root;
 
     @Override
     public Value get(Key key) {
@@ -21,7 +28,65 @@ public class AvlBst<Key extends Comparable<Key>, Value>
 
     @Override
     public void put(Key key, Value value) {
-        throw new UnsupportedOperationException("Implement me");
+        root = put(root, key, value);
+    }
+
+    private Node put(Node x, Key key, Value value) {
+        if (x == null) {
+            return new Node(key, value, 1);
+        }
+
+        if (key.compareTo(x.key) < 0) {
+            x.left = put(x.left, key, value);
+        } else if (key.compareTo(x.key) > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+
+        fixHeight(x);
+        x = balance(x);
+        return x;
+    }
+
+    private Node balance(Node x) {
+        if (factor(x) == 2) {
+            if (factor(x.left) < 0) {
+                x.left = rotateLeft(x.left);
+            }
+            return rotateRight(x);
+        }
+
+        if (factor(x) == -2) {
+            if (factor(x.right) > 0) {
+                x.right = rotateRight(x.right);
+            }
+            return rotateLeft(x);
+        }
+
+        return x;
+    }
+
+    private int factor(Node x) {
+        return height(x.left) - height(x.right);
+    }
+
+    private Node rotateRight(Node x) {
+        Node left = x.left;
+        x.left = left.right;
+        left.right = x;
+        fixHeight(x);
+        fixHeight(left);
+        return left;
+    }
+
+    private Node rotateLeft(Node x) {
+        Node right = x.right;
+        x.right = right.left;
+        right.left = x;
+        fixHeight(x);
+        fixHeight(right);
+        return right;
     }
 
     @Override
@@ -66,6 +131,22 @@ public class AvlBst<Key extends Comparable<Key>, Value>
 
     @Override
     public int height() {
-        throw new UnsupportedOperationException("Implement me");
+        return height(root);
+    }
+
+    private int height(Node x) {
+        return x == null ? 0 : x.height;
+    }
+
+    private void fixHeight(Node x) {
+        x.height = 1 + max(height(x.left), height(x.right));
+    }
+
+    private int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 }
