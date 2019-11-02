65a40184359043a2afd18aa619d70afb1fefc851
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-11-02 11:59:24 +0300

    Implementing avl-tree

--- src/main/java/ru/mail/polis/ads/bst/AvlBst.java
+++ src/main/java/ru/mail/polis/ads/bst/AvlBst.java
@@ -4,68 +4,282 @@ package ru.mail.polis.ads.bst;
  * AVL implementation of binary search tree.
  */
 public class AvlBst<Key extends Comparable<Key>, Value>
-        implements Bst<Key, Value> {
-    
+    implements Bst<Key, Value> {
+
+    private int size;
+
     private class Node {
-        Key key;
-        Value value;
-        Node left;
-        Node right;
-        int height;
+        private Key key;
+        private Value value;
+        private Node left;
+        private Node right;
+
+        private Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        private int getHeight() {
+            if (left == null) {
+                if (right == null) {
+                    return 1;
+                }
+                return 1 + right.getHeight();
+            }
+            if (right == null) {
+                return 1 + left.getHeight();
+            }
+            return 1 + Math.max(left.getHeight(), right.getHeight());
+        }
     }
+
+    private Node top;
 
     @Override
     public Value get(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        Node node = findNode(top, key);
+        if (node == null) {
+            return null;
+        }
+        return node.value;
+    }
+
+    private Node findNode(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) == 0) {
+            return node;
+        }
+        if (key.compareTo(node.key) < 0) {
+            return findNode(node.left, key);
+        }
+        return findNode(node.right, key);
     }
 
     @Override
     public void put(Key key, Value value) {
-        throw new UnsupportedOperationException("Implement me");
+        top = put(top, key, value);
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(node.key) == 0) {
+            node.value = value;
+        } else if (key.compareTo(node.key) < 0) {
+            node.left = put(node.left, key, value);
+        } else {
+            node.right = put(node.right, key, value);
+        }
+        return balance(node);
     }
 
     @Override
     public Value remove(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        Node deletedParent = null;
+        Node deleted = top;
+        while (deleted != null && key.compareTo(deleted.key) != 0) {
+            deletedParent = deleted;
+            if (key.compareTo(deleted.key) < 0) {
+                deleted = deleted.left;
+            } else {
+                deleted = deleted.right;
+            }
+        }
+
+        if (deleted == null) {
+            return null;
+        }
+
+        size--;
+
+        if (deleted.right == null) {
+            if (deletedParent == null) {
+                top = null;
+            } else {
+                if (deletedParent.left == deleted) {
+                    deletedParent.left = deleted.left;
+                } else {
+                    deletedParent.right = deleted.left;
+                }
+            }
+            balance(deletedParent);
+            return deleted.value;
+        }
+
+        Node leastRightParent = deleted;
+        Node leastRight = deleted.right;
+
+        while (leastRight.left != null) {
+            leastRightParent = leastRight;
+            leastRight = leastRight.left;
+        }
+
+        leastRightParent.left = leastRight.right;
+        leastRight.left = deleted.left;
+        if (deleted.right != leastRight) {
+            leastRight.right = deleted.right;
+        }
+
+        if (deletedParent == null) {
+            top = leastRight;
+        } else if (deletedParent.left == deleted) {
+            deletedParent.left = leastRight;
+        } else {
+            deletedParent.right = leastRight;
+        }
+
+        balance(leastRight);
+        return deleted.value;
     }
+
+    private Node min(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
 
     @Override
     public Key min() {
-        throw new UnsupportedOperationException("Implement me");
+        if (top == null) {
+            return null;
+        }
+        return min(top).key;
     }
 
     @Override
     public Value minValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (top == null) {
+            return null;
+        }
+        return min(top).value;
+    }
+
+    private Node max(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return max(node.right);
     }
 
     @Override
     public Key max() {
-        throw new UnsupportedOperationException("Implement me");
+        if (top == null) {
+            return null;
+        }
+        return max(top).key;
     }
 
     @Override
     public Value maxValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (top == null) {
+            return null;
+        }
+        return max(top).value;
+    }
+
+    private Node floor(Node node, Key key) {
+        if (node.right != null && key.compareTo(node.right.key) > 0) {
+            return floor(node.right, key);
+        }
+        if (node.left != null && key.compareTo(node.key) < 0) {
+            return floor(node.left, key);
+        }
+        if (key.compareTo(node.key) < 0) {
+            return null;
+        }
+        return node;
     }
 
     @Override
     public Key floor(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        if (top == null) {
+            return null;
+        }
+        Node floor = floor(top, key);
+        if (floor == null) {
+            return null;
+        }
+        return floor.key;
+    }
+
+    private Node ceil(Node node, Key key) {
+        if (node.left != null && key.compareTo(node.left.key) < 0) {
+            return ceil(node.left, key);
+        }
+        if (node.right != null && key.compareTo(node.key) > 0) {
+            return ceil(node.right, key);
+        }
+        if (key.compareTo(node.key) > 0) {
+            return null;
+        }
+        return node;
     }
 
     @Override
     public Key ceil(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        if (top == null) {
+            return null;
+        }
+        Node ceil = ceil(top, key);
+        if (ceil == null) {
+            return null;
+        }
+        return ceil.key;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Implement me");
+        return size;
     }
 
     @Override
     public int height() {
-        throw new UnsupportedOperationException("Implement me");
+        if (top == null) {
+            return 0;
+        }
+        return top.getHeight();
+    }
+
+    private Node rotateRight(Node node) {
+        Node leftNode = node.left;
+        node.left = leftNode.right;
+        leftNode.right = node;
+        return leftNode;
+    }
+
+    private Node rotateLeft(Node node) {
+        Node rightNode = node.right;
+        node.right = rightNode.left;
+        rightNode.left = node;
+        return rightNode;
+    }
+
+    private int factor(Node node) {
+        return (node.left == null ? 0 : node.left.getHeight()) - (node.right == null ? 0 : node.right.getHeight());
+    }
+
+    private Node balance(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (factor(node) == -2) {
+            if (factor(node.right) > 0) {
+                node.right = rotateRight(node.right);
+            }
+            return rotateLeft(node);
+        }
+        if (factor(node) == 2) {
+            if (factor(node.left) < 0) {
+                node.left = rotateLeft(node.left);
+            }
+            return rotateRight(node);
+        }
+        return node;
     }
 }
--- src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java
+++ src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.ads.bst;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,14 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
  * Basic binary search tree invariants.
  */
 class BstBaseTest {
-    
-    Bst<String, String> newBst() {
-        return new AvlBst<>();
-    } 
-    
+
+    private Bst<String, String> bst;
+
+    @BeforeEach
+    void setUp() {
+        bst = new AvlBst<>();
+    }
+
     @Test
     void emptyBst() {
-        Bst<String, String> bst = newBst();
         assertNull(bst.get(""));
         assertNull(bst.get("some key"));
         assertEquals(0, bst.size());
@@ -24,7 +27,6 @@ class BstBaseTest {
 
     @Test
     void orderedOnEmpty() {
-        Bst<String, String> bst = newBst();
         assertNull(bst.ceil("some key"));
         assertNull(bst.floor("some key"));
 
@@ -37,25 +39,156 @@ class BstBaseTest {
 
     @Test
     void put() {
-        Bst<String, String> bst = newBst();
         bst.put("foo", "bar");
         
         assertEquals("bar", bst.get("foo"));
-        
         assertEquals(1, bst.size());
+
+        bst.put("key1", "value1");
+
+        assertEquals("bar", bst.get("foo"));
+        assertEquals("value1", bst.get("key1"));
+        assertEquals(2, bst.size());
+
+        bst.put("key2", "value2");
+
+        assertEquals("bar", bst.get("foo"));
+        assertEquals("value1", bst.get("key1"));
+        assertEquals("value2", bst.get("key2"));
+        assertEquals(3, bst.size());
+
+        bst.put("key3", "value3");
+
+        assertEquals("bar", bst.get("foo"));
+        assertEquals("value1", bst.get("key1"));
+        assertEquals("value2", bst.get("key2"));
+        assertEquals("value3", bst.get("key3"));
+        assertEquals(4, bst.size());
+    }
+
+    @Test
+    void put_TreeBalancesItself() {
+        bst.put("key1", "value");
         assertEquals(1, bst.height());
+
+        bst.put("key2", "value");
+        assertEquals(2, bst.height());
+
+        bst.put("key3", "value");
+        assertEquals(2, bst.height());
+
+        bst.put("key4", "value");
+        assertEquals(3, bst.height());
+
+        bst.remove("key2");
+        assertEquals(2, bst.height());
+    }
+
+    @Test
+    void get() {
+        assertNull(bst.get("non-existent"));
+
+        bst.put("key", "value");
+
+        assertNull(bst.get("non-existent"));
+        assertEquals("value", bst.get("key"));
     }
 
     @Test
     void replace() {
-        Bst<String, String> bst = newBst();
         bst.put("foo", "bar");
         bst.put("foo", "bee");
 
         assertEquals("bee", bst.get("foo"));
 
         assertEquals(1, bst.size());
-        assertEquals(1, bst.height());
     }
-    
+
+    @Test
+    void remove() {
+        assertNull(bst.remove("non-existent"));
+
+        bst.put("key1", "value1");
+        bst.put("key2", "value2");
+
+        assertEquals("value2", bst.remove("key2"));
+        assertEquals(1, bst.size());
+        assertNull(bst.get("key2"));
+        assertEquals("value1", bst.get("key1"));
+
+        bst.put("key2", "value2");
+
+        assertEquals("value1", bst.remove("key1"));
+        assertEquals(1, bst.size());
+        assertNull(bst.get("key1"));
+        assertEquals("value2", bst.get("key2"));
+
+        assertEquals("value2", bst.remove("key2"));
+        assertEquals(0, bst.size());
+        assertNull(bst.get("key2"));
+    }
+
+    @Test
+    void min() {
+        assertNull(bst.min());
+
+        bst.put("key3", "value3");
+
+        assertEquals("key3", bst.min());
+        assertEquals("value3", bst.minValue());
+
+        bst.put("key1", "value1");
+
+        assertEquals("key1", bst.min());
+        assertEquals("value1", bst.minValue());
+
+        bst.put("key2", "value2");
+
+        assertEquals("key1", bst.min());
+        assertEquals("value1", bst.minValue());
+    }
+
+    @Test
+    void max() {
+        assertNull(bst.max());
+
+        bst.put("key1", "value1");
+
+        assertEquals("key1", bst.max());
+        assertEquals("value1", bst.maxValue());
+
+        bst.put("key3", "value3");
+
+        assertEquals("key3", bst.max());
+        assertEquals("value3", bst.maxValue());
+
+        bst.put("key2", "value2");
+
+        assertEquals("key3", bst.max());
+        assertEquals("value3", bst.maxValue());
+    }
+
+    @Test
+    void floor() {
+        assertNull(bst.floor("key"));
+
+        bst.put("key1", "value1");
+        bst.put("key2", "value2");
+
+        assertNull(bst.floor("key0"));
+        assertEquals("key1", bst.floor("key1"));
+        assertEquals("key2", bst.floor("key3"));
+    }
+
+    @Test
+    void ceil() {
+        assertNull(bst.ceil("key"));
+
+        bst.put("key1", "value1");
+        bst.put("key2", "value2");
+
+        assertNull(bst.ceil("key3"));
+        assertEquals("key2", bst.ceil("key2"));
+        assertEquals("key1", bst.ceil("key0"));
+    }
 }
