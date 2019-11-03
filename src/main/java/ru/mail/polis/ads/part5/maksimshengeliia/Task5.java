14fc603bed3cc6075ccee07b7603a200bcb430a8
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-11-04 00:03:23 +0300

    Complete part 5

--- src/main/java/ru/mail/polis/ads/part5/maksimshengeliia/Task5.java
+++ src/main/java/ru/mail/polis/ads/part5/maksimshengeliia/Task5.java
@@ -1,14 +1,98 @@
 package ru.mail.polis.ads.part5.maksimshengeliia;
 
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 /*
-*
+*   https://www.e-olymp.com/ru/submissions/6018426
 * */
 public class Task5 {
-    public static void main(String[] strings) {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        //int n = new Scanner(System.in).nextByte();
 
+        int n = in.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = i + 1;
+        }
+
+        showArray(array, out);
+        lab: while (true) {
+
+            if (n == 1) break;
+
+            int i = array.length - 1;
+            while (array[i] < array[i - 1]) {
+                i--;
+                if (i == 0) {
+                    break lab;
+                }
+            }
+
+            int j = array.length - 1;
+            while (array[j] < array[i - 1]) {
+                j--;
+            }
+
+            swap(array, j, i - 1);
+            int left = i;
+            int right = array.length - 1;
+            while (right > left) {
+                swap(array, right--, left++);
+            }
+            showArray(array, out);
+        }
+    }
+
+    private static void swap(int[] a, int l, int r) {
+        int temp = a[l];
+        a[l] = a[r];
+        a[r] = temp;
+    }
+
+    private static void showArray(int[] array, PrintWriter out) {
+        for (int value : array) {
+            out.print(value + " ");
+        }
+        out.println();
+    }
+
+
+    private Task5() {
+        // Should not be instantiated
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
     }
 }
 
