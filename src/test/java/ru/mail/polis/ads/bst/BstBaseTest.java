614150c54821f5c7e0a536f9f98875d09a8a6179
Nekobitlz <jokers.bit@gmail.com>
2019-11-03 04:00:11 +0300

    Add more tests for AvlBst

--- src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java
+++ src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java
@@ -36,6 +36,37 @@ class BstBaseTest {
     }
 
     @Test
+    void orderedOnNotEmpty() {
+        Bst<String, String> bst = newBst();
+
+        bst.put("key1", "value1");
+        bst.put("key2", "value2");
+        bst.put("key3", "value3");
+        bst.put("key4", "value4");
+        bst.put("key5", "value5");
+
+        assertEquals("key2", bst.ceil("key2"));
+        assertEquals("key4", bst.ceil("key32"));
+        assertEquals("key1", bst.ceil("key0"));
+        assertEquals("key3", bst.ceil("key21"));
+        assertEquals("key5", bst.ceil("key45"));
+        assertNull(bst.ceil("key68"));
+
+        assertEquals("key5", bst.floor("key5"));
+        assertEquals("key3", bst.floor("key32"));
+        assertEquals("key4", bst.floor("key45"));
+        assertEquals("key5", bst.floor("key68"));
+        assertEquals("key2", bst.floor("key21"));
+        assertNull(bst.floor("key0"));
+
+        assertEquals("key1", bst.min());
+        assertEquals("key5", bst.max());
+
+        assertEquals("value1", bst.minValue());
+        assertEquals("value5", bst.maxValue());
+    }
+
+    @Test
     void put() {
         Bst<String, String> bst = newBst();
         bst.put("foo", "bar");
@@ -44,6 +75,31 @@ class BstBaseTest {
         
         assertEquals(1, bst.size());
         assertEquals(1, bst.height());
+
+        bst.put("key1", "value1");
+        bst.put("key2", "value2");
+        bst.put("key2", "value3");
+
+        assertNull(bst.get("value1"));
+        assertNull(bst.get("value3"));
+
+        assertEquals("value1", bst.get("key1"));
+        assertEquals("value3", bst.get("key2"));
+
+        assertEquals(3, bst.size());
+        assertEquals(2, bst.height());
+
+        bst.put("key3", "value4");
+        bst.put("key4", "value5");
+
+        assertNull(bst.get("value4"));
+        assertNull(bst.get("value5"));
+
+        assertEquals("value4", bst.get("key3"));
+        assertEquals("value5", bst.get("key4"));
+
+        assertEquals(5, bst.size());
+        assertEquals(3, bst.height());
     }
 
     @Test
@@ -57,5 +113,4 @@ class BstBaseTest {
         assertEquals(1, bst.size());
         assertEquals(1, bst.height());
     }
-    
 }
