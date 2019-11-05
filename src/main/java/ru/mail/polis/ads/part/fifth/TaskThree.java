a50acacba39b02a01b80ad723ebb0f38b10d8053
v.ivlev <v.ivlev@geoscan.aero>
2019-11-05 16:12:01 +0300

    Task 3
    See: https://www.e-olymp.com/ru/submissions/6033534

---
+++ src/main/java/ru/mail/polis/ads/part/fifth/TaskThree.java
@@ -0,0 +1,79 @@
+package ru.mail.polis.ads.part.fifth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @author v.ivlev
+ * https://www.e-olymp.com/ru/submissions/6033534
+ */
+public class TaskThree {
+
+    private static void solve(FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] d = new int[n];
+        int[] array = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        d[0] = 1;
+        for (int i = 1; i < n; i++) {
+            max = 0;
+            for (int j = 0; j < i; j++) {
+                if (array[j] != 0 &&
+                        d[j] > max &&
+                        array[i] % array[j] == 0) {
+                    max = d[j];
+                }
+            }
+            d[i] = max + 1;
+        }
+        out.print(max(d, n));
+    }
+
+    private static int max(int[] array, int n) {
+        int max = array[0];
+        for (int i = 1; i < n; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
