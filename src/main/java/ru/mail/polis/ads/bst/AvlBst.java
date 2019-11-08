a28f28ddf1e1f8a45309d40512f71939ec69c9d2
gogun <gusarov2000@gmail.com>
2019-11-08 17:52:41 +0300

    add all methods

--- src/main/java/ru/mail/polis/ads/bst/AvlBst.java
+++ src/main/java/ru/mail/polis/ads/bst/AvlBst.java
@@ -16,9 +16,10 @@ public class AvlBst<Key extends Comparable<Key>, Value> implements Bst<Key, Valu
         Node right;
         int height;
 
-        public Node(Key key, Value value) {
+        public Node(Key key, Value value, int height) {
             this.key = key;
             this.value = value;
+            this.height = height;
         }
     }
 
@@ -28,7 +29,7 @@ public class AvlBst<Key extends Comparable<Key>, Value> implements Bst<Key, Valu
         size = 0;
     }
 
-    public Value get(Node node, Key key) {
+    private Value get(Node node, Key key) {
         if (node == null)
             return null;
         if(key.compareTo(node.key) < 0)
@@ -44,10 +45,54 @@ public class AvlBst<Key extends Comparable<Key>, Value> implements Bst<Key, Valu
         return get(root, key);
     }
     //my
-    Node put(Key key, Value value, Node node) {
+
+    private Node rotateRight(Node node) {
+        Node left = node.left;
+        node.left = left.right;
+        left.right = node;
+        fixHeight(node);
+        fixHeight(left);
+
+        return left;
+    }
+
+    private Node rotateLeft(Node node) {
+        Node right = node.right;
+        node.right = right.left;
+        right.left = node;
+        fixHeight(node);
+        fixHeight(right);
+
+        return right;
+    }
+
+    private Node balance(Node node) {
+        if (factor(node) == 2) {
+            if (factor(node.left) < 0)
+                node.left = rotateLeft(node.left);
+            return rotateRight(node.right);
+        }
+        if (factor(node) == -2) {
+            if (factor(node.right) > 0)
+                node.right = rotateRight(node.right);
+            return rotateLeft(node.left);
+        }
+
+        return node;
+    }
+
+    private int factor(Node node) {
+        return height(node.left) - height(node.right);
+    }
+
+    private void fixHeight(Node node) {
+        node.height = 1 + Math.max(height(node.right), height(node.left));
+    }
+
+    private Node put(Key key, Value value, Node node) {
         if (node == null) {
             ++size;
-            return new Node(key, value);
+            return new Node(key, value, 1);
         }
 
         if (key.compareTo(node.key) < 0)
@@ -56,6 +101,8 @@ public class AvlBst<Key extends Comparable<Key>, Value> implements Bst<Key, Valu
             node.right = put(key, value, node.right);
         else
             node.value = value;
+        fixHeight(node);
+        node = balance(node);
 
         return node;
     }
@@ -66,39 +113,148 @@ public class AvlBst<Key extends Comparable<Key>, Value> implements Bst<Key, Valu
         root = put(key, value, root);
     }
 
+    //my
+    private Node min(Node node) {
+        if (node == null)
+            return null;
+
+        while (node.left != null) {
+            node = node.left;
+        }
+
+        return node;
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null)
+            return node.right;
+        node.left = deleteMin(node.left);
+
+        return node;
+    }
+
+    private Node innerDelete(Node node) {
+        if (node.right == null)
+            return node.left;
+        if (node.left == null)
+            return node.right;
+        Node tmp_node = node;
+        node = min(tmp_node.right);
+        node.right = deleteMin(tmp_node.right);
+        node.left = tmp_node.left;
+
+        --size;
+        return node;
+    }
+
+    private Node delete(Node node, Key key) {
+        if (node == null)
+            return null;
+
+        if (key.compareTo(node.key) < 0)
+            node.left = delete(node.left, key);
+        if (key.compareTo(node.key) > 0)
+            node.right = delete(node.right, key);
+        if (key.compareTo(node.key) == 0)
+            node = innerDelete(node);
+
+        return node;
+    }
+    //enfmy
     @Override
     public void remove(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        root = delete(root, key);
     }
 
     @Override
     public Key min() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null)
+            return null;
+
+        Node tmp_node;
+        tmp_node = root;
+        while (tmp_node.left != null) {
+            tmp_node = tmp_node.left;
+        }
+
+        return tmp_node.key;
     }
 
     @Override
     public Value minValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null)
+            return null;
+
+        Node tmp_node;
+        tmp_node = root;
+        while (tmp_node.left != null) {
+            tmp_node = tmp_node.left;
+        }
+
+        return tmp_node.value;
     }
 
     @Override
     public Key max() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null)
+            return null;
+
+        Node tmp_node;
+        tmp_node = root;
+        while (tmp_node.right != null) {
+            tmp_node = tmp_node.right;
+        }
+
+        return tmp_node.key;
     }
 
     @Override
     public Value maxValue() {
-        throw new UnsupportedOperationException("Implement me");
-    }
+        if (root == null)
+            return null;
 
+        Node tmp_node;
+        tmp_node = root;
+        while (tmp_node.right != null) {
+            tmp_node = tmp_node.right;
+        }
+
+        return tmp_node.value;
+    }
+    //my
+    private Node getNode(Node node, Key key) {
+        if (node == null)
+            return null;
+        if(key.compareTo(node.key) < 0)
+            get(node.left, key);
+        else if (key.compareTo(node.key) > 0)
+            get(node.right, key);
+
+        return node;
+    }
+    //endmy
     @Override
     public Key floor(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        Node tmp_node = getNode(root, key);
+        if (tmp_node == null || tmp_node.left == null)
+            return null;
+        tmp_node = tmp_node.left;
+        while (tmp_node.right != null)
+            tmp_node = tmp_node.right;
+
+        return tmp_node.key;
     }
 
     @Override
     public Key ceil(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        Node tmp_node = getNode(root, key);
+        if (tmp_node == null || tmp_node.right == null)
+            return null;
+        tmp_node = tmp_node.right;
+        while (tmp_node.left != null)
+            tmp_node = tmp_node.left;
+
+        return tmp_node.key;
     }
 
     @Override
@@ -106,8 +262,14 @@ public class AvlBst<Key extends Comparable<Key>, Value> implements Bst<Key, Valu
         return size;
     }
 
+    //my
+    int height(Node x) {
+        return x == null ? 0 : x.height;
+    }
+    //endmy
+
     @Override
     public int height() {
-        throw new UnsupportedOperationException("Implement me");
+        return height(root);
     }
 }
