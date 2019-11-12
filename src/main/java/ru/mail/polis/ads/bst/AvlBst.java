dd78f52ffeffa7ebea7c5dcd11a0d55db1c03527
Saprykin Vitaly <sapryk1n.vitaliy@yandex.ru>
2019-11-12 22:33:08 +0300

    Remaining: height

--- src/main/java/ru/mail/polis/ads/bst/AvlBst.java
+++ src/main/java/ru/mail/polis/ads/bst/AvlBst.java
@@ -5,48 +5,175 @@ package ru.mail.polis.ads.bst;
  */
 public class AvlBst<Key extends Comparable<Key>, Value>
         implements Bst<Key, Value> {
-    
+
+    private Node root;
+    private int size;
+
     private class Node {
         Key key;
         Value value;
         Node left;
         Node right;
         int height;
+
+        Node(Key key, Value value, int height) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
     @Override
     public Value get(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        if (size == 0) return null;
+        return get(root, key);
+    }
+
+    private Value get(Node node, Key key) {
+        if (node == null) return null;
+        if (key.compareTo(node.key) < 0)
+            return (get(node.left, key));
+        if (key.compareTo(node.key) > 0)
+            return (get(node.right, key));
+        return node.value;
     }
 
     @Override
     public void put(Key key, Value value) {
-        throw new UnsupportedOperationException("Implement me");
+        root = put(root, key, value);
+        size++;
     }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null) {
+            return new Node(key, value, 1);
+        }
+        if (key.compareTo(node.key) < 0)
+            node.left = put(node.left, key, value);
+        else if (key.compareTo(node.key) > 0)
+            node.right = put(node.right, key, value);
+        else {
+            node.value = value;
+        }
+        /*fixHeight(node);
+        node = balance(node);*/
+        return node;
+    }
+
+    private boolean deleted = false;
 
     @Override
     public void remove(Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        if (size == 1) {
+            if (root.key == key) {
+                root = null;
+                size--;
+            }
+        } else {
+            root = remove(root, key);
+        }
+        deleted = false;
+    }
+
+
+    private Node remove(Node node, Key key) {
+        if (node == null) return null;
+
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+        }
+        if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+        } else if (!deleted) {
+            deleted = true;
+            size--;
+            if (node.left != null && node.right != null){
+                Node xNode = node;
+                node = min(xNode.right);
+                node.right = deleteMin(xNode.right);
+                node.left = xNode.left;
+            }
+            else if (node.left != null) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+            /*fixedNodeHeight(node);
+            return balanceNode(node);*/
+        return node;
+    }
+
+    /*private Node innerDelete(Node node) {
+        if (node.right == null) {
+            node = node.left;
+            return null;
+        }
+        if (node.left == null) {
+            node = node.right;
+            return null;
+        }
+        Node xNode = node;
+        node = min(xNode.right);
+        node.right = deleteMin(xNode.right);
+        node.left = xNode.left;
+        return node;
+    }*/
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) return node.right;
+
+        node.left = deleteMin(node.left);
+
+        return node;
     }
 
     @Override
     public Key min() {
-        throw new UnsupportedOperationException("Implement me");
+        if (size == 0) return null;
+        Node foundNode = min(root);
+        return foundNode.key;
     }
+
+    private Node min(Node node) {
+        if (node == null) return null;
+
+        while (node.left != null) {
+            node = node.left;
+        }
+
+        return node;
+    }
+
 
     @Override
     public Value minValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (size == 0) return null;
+        Node foundNode = min(root);
+        return foundNode.value;
     }
 
     @Override
     public Key max() {
-        throw new UnsupportedOperationException("Implement me");
+        if (size == 0) return null;
+        Node foundNode = max(root);
+        return foundNode.key;
+    }
+
+    private Node max(Node node) {
+        if (node == null) return null;
+
+        while (node.right != null) {
+            node = node.right;
+        }
+
+        return node;
     }
 
     @Override
     public Value maxValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (size == 0) return null;
+        Node foundNode = max(root);
+        return foundNode.value;
     }
 
     @Override
@@ -61,7 +188,7 @@ public class AvlBst<Key extends Comparable<Key>, Value>
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Implement me");
+        return size;
     }
 
     @Override
