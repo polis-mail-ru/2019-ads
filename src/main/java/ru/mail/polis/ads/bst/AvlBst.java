19e3a3a2effaeca211bbbdb7106399a5521d6dc6
valer1435 <valp998@gmail.com>
2019-11-14 17:03:46 +0300

    red-black tree

--- src/main/java/ru/mail/polis/ads/bst/AvlBst.java
+++ src/main/java/ru/mail/polis/ads/bst/AvlBst.java
@@ -9,24 +9,109 @@ import org.jetbrains.annotations.Nullable;
 public class AvlBst<Key extends Comparable<Key>, Value>
         implements Bst<Key, Value> {
 
+    static final boolean RED = true;
+    static final boolean BLACK = false;
+    public Node root;
+
     private class Node {
         Key key;
         Value value;
         Node left;
         Node right;
         int height;
+        boolean color;
+
+        Node(Key key, Value value, int height, boolean color){
+            this.key = key;
+            this.value = value;
+            this.height = height;
+            this.color = color;
+        }
+
     }
+    boolean isRed(Node x) {
+        return x != null && x.color == RED;
+    }
+
 
     @Nullable
     @Override
     public Value get(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return get(root, key);
+    }
+    Value get(Node x, Key key){
+        if (x == null){
+            return null;
+        }
+        if (key.compareTo(x.key) > 0){
+            return get(x.right, key);
+        }
+        if (key.compareTo(x.key) < 0){
+            return get(x.left, key);
+        }
+        return x.value;
     }
 
     @Override
     public void put(@NotNull Key key, @NotNull Value value) {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null){
+            root = new Node(key, value, 1, BLACK);
+            return;
+        }
+        root = put(root, key, value);
+        root.color = BLACK;
     }
+    Node put(Node x, Key key, Value value){
+        if (x == null){
+            return new Node(key, value, 1, RED);
+
+        }
+        if (key.compareTo(x.key) > 0){
+            x.right = put(x.right, key, value);
+        }
+        else if (key.compareTo(x.key) < 0){
+            x.left = put(x.left, key, value);
+        }
+        else{
+            x.value = value;
+        }
+        x = fixUp(x);
+        fixHeight(x);
+        return x;
+    }
+    Node fixUp(Node x){
+        if (isRed(x.right) && !isRed(x.left)){
+            x = rotateLeft(x);
+        }if (isRed(x.left) && isRed(x.left.left)){
+            x = rotateRight(x);
+        }if (isRed(x.right) && isRed(x.left)){
+            flipColors(x);
+        }
+        return x;
+    }
+    Node rotateLeft(Node x){
+        Node right = x.right;
+        x.right = right.left;
+        right.left = x;
+        right.color = x.color;
+        x.color = RED;
+        return right;
+    }
+    Node rotateRight(Node x){
+        Node left = x.left;
+        x.left = left.right;
+        left.right = x;
+        left.color = x.color;
+        x.color = RED;
+        return left;
+    }
+    Node flipColors(Node x){
+        x.color = !x.color;
+        x.right.color = !x.right.color;
+        x.left.color = !x.left.color;
+        return x;
+    }
+
 
     @Nullable
     @Override
@@ -37,46 +122,138 @@ public class AvlBst<Key extends Comparable<Key>, Value>
     @Nullable
     @Override
     public Key min() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null){
+            return null;
+        }else {
+            return min(root).key;
+        }
+    }
+    Node min(Node x){
+        if (x.left != null){
+            return min(x.left);
+        }else{
+            return x;
+        }
     }
 
     @Nullable
     @Override
     public Value minValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null){
+            return null;
+        }else {
+            return get(min(root).key);
+        }
     }
+
 
     @Nullable
     @Override
     public Key max() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null){
+            return null;
+        }else {
+            return max(root).key;
+        }
+    }
+    Node max(Node x){
+        if (x.right != null){
+            System.out.println(x+" "+x.right);
+            return max(x.right);
+        }else{
+            return x;
+        }
     }
 
     @Nullable
     @Override
     public Value maxValue() {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null){
+            return null;
+        }else {
+            return get(max(root).key);
+        }
     }
 
     @Nullable
     @Override
     public Key floor(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        if (root == null){
+            return null;
+        }
+        return  floor(root, key);
+    }
+    Key floor(Node x, Key key){
+        if (key.compareTo(x.key) < 0){
+            return max(x.left).key;
+        }else if (key == x.key){
+            return x.key;
+        }return floor(x.left, key);
     }
 
-    @Nullable
+
+        @Nullable
     @Override
     public Key ceil(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+            if (root == null){
+                return null;
+            }
+            return ceil(root, key);
+        }
+
+    Key ceil(Node x, Key key){
+        if (key.compareTo(x.key) > 0){
+            return min(x.right).key;
+        }else if (key == x.key){
+            return x.key;
+        }return ceil(x.right, key);
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Implement me");
+        return size(root);
     }
+    int size(Node x){
+        if (x == null){
+            return 0;
+        }
+        if (x.left == null && x.right == null ){
+            return 1;
+        }
+        return size(x.left)+size(x.right);
+    }
+
 
     @Override
     public int height() {
-        throw new UnsupportedOperationException("Implement me");
+        return height(root);
     }
+    int height(Node x){
+        if (x == null){
+            return 0;
+        }
+        return x.height;
+    }
+    void fixHeight(Node x){
+        if (x.left == null){
+            if (x.right == null){
+                x.height = height(x);
+            }else {
+                x.height = height(x.right)+1;
+            }
+        }else if (x.right == null){
+            if (!isRed(x.left))
+                x.height = height(x.left)+1;
+            else{
+                x.height = height(x);
+            }
+        }
+        else{
+            if (!isRed(x.left))
+                x.height = Math.max(height(x.left), height(x.right))+1;
+            else{
+                x.height = height(x.right);
+        }
+    }
+}
 }
--- src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java
+++ src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java
@@ -11,51 +11,68 @@ class BstBaseTest {
     
     Bst<String, String> newBst() {
         return new AvlBst<>();
-    } 
-    
+    }
+
+
     @Test
-    void emptyBst() {
-        Bst<String, String> bst = newBst();
-        assertNull(bst.get(""));
-        assertNull(bst.get("some key"));
-        assertEquals(0, bst.size());
-        assertEquals(0, bst.height());
+    void remove() {
+        Bst<Integer, Integer> bst = new AvlBst<>();
+        final int elementsCount = 1024;
+        for (int i = 1; i <= elementsCount; ++i) {
+            bst.put(i, i);
+        }
+        for (int i = 1; i <= elementsCount; i += 2) {
+            assertEquals(i, bst.get(i));
+            bst.remove(i);
+            assertNull(bst.get(i));
+            assertEquals(i + 1, bst.get(i + 1));
+        }
+        assertEquals(elementsCount / 2, bst.size());
+        assertTrue(Math.abs(Math.log(elementsCount / 2)/Math.log(2) - bst.height()) <= 1.0001);
     }
 
     @Test
-    void orderedOnEmpty() {
-        Bst<String, String> bst = newBst();
-        assertNull(bst.ceil("some key"));
-        assertNull(bst.floor("some key"));
-
-        assertNull(bst.min());
-        assertNull(bst.max());
-
-        assertNull(bst.minValue());
-        assertNull(bst.maxValue());
+    void floor() {
+        Bst<Integer, Integer> bst = new AvlBst<>();
+        final int elementsCount = 1024;
+        for (int i = 1; i <= elementsCount; ++i) {
+            bst.put(i, i);
+        }
+        bst.remove(elementsCount / 2);
+        assertEquals(elementsCount / 2 - 1, bst.floor(elementsCount / 2));
     }
 
     @Test
-    void put() {
-        Bst<String, String> bst = newBst();
-        bst.put("foo", "bar");
-        
-        assertEquals("bar", bst.get("foo"));
-        
-        assertEquals(1, bst.size());
-        assertEquals(1, bst.height());
+    void ceil() {
+        Bst<Integer, Integer> bst = new AvlBst<>();
+        final int elementsCount = 1024;
+        for (int i = 1; i <= elementsCount; ++i) {
+            bst.put(i, i);
+        }
+        bst.remove(elementsCount / 2);
+        assertEquals(elementsCount / 2 + 1, bst.ceil(elementsCount / 2));
     }
 
     @Test
-    void replace() {
-        Bst<String, String> bst = newBst();
-        bst.put("foo", "bar");
-        bst.put("foo", "bee");
+    void max() {
+        Bst<Integer, Integer> bst = new AvlBst<>();
+        final int elementsCount = 1024;
+        for (int i = 1; i <= elementsCount; ++i) {
+            bst.put(i, i * 1000);
+        }
+        assertEquals(elementsCount, bst.max());
+        assertEquals(elementsCount * 1000, bst.maxValue());
+    }
 
-        assertEquals("bee", bst.get("foo"));
-
-        assertEquals(1, bst.size());
-        assertEquals(1, bst.height());
+    @Test
+    void min() {
+        Bst<Integer, Integer> bst = new AvlBst<>();
+        final int elementsCount = 1024;
+        for (int i = 1; i <= elementsCount; ++i) {
+            bst.put(i, i * 1000);
+        }
+        assertEquals(1, bst.min());
+        assertEquals(1 * 1000, bst.minValue());
     }
     
 }
