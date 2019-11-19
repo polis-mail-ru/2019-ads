cb65a9b4864065aa2e8295d9435185bd812775a8
DanilA-Barsik1337 <danil.antonov.96@list.ru>
2019-11-19 19:01:13 +0300

    part_5

---
+++ src/main/java/ru/mail/polis/ads/part_1.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+//https://www.e-olymp.com/ru/submissions/6148668
+public class part_1{
+
+    private static void solve(FastScanner in, final PrintWriter out) {
+        double value = Double.parseDouble(in.nextLine());
+        double left = 0;
+        double right = value;
+        double e = 0.000001;
+        double x;
+        double y;
+
+        do {
+            x = (left + right) / 2;
+            y = x * x + Math.sqrt(x);
+            if (y > value) {
+                right = x;
+            } else {
+                left = x;
+            }
+        } while (Math.abs(y - value) > e);
+
+        out.print(String.format("%.6f", x));
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        String nextLine() {
+            return next();
+        }
+    }
+
+    public static void main(final String[] arg) {
+        FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
---
+++ src/main/java/ru/mail/polis/ads/part_4.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class part_4 {
+
+    private static void solve(FastScanner in, final PrintWriter out) {
+        String input = in.nextLine();
+        String templateOne;
+        String templateTwo;
+        int n = 0;
+        int m = 0;
+        boolean[][] mask;
+        if (input.contains("*") || input.contains("?")) {
+            templateOne = input;
+            templateTwo = in.nextLine();
+        } else {
+            templateOne = in.nextLine();
+            templateTwo = input;
+        }
+        n = templateTwo.length();
+        m = templateOne.length();
+        mask = new boolean[n + 1][m + 1];
+        mask[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (templateTwo.charAt(i - 1) == templateOne.charAt(j - 1) || templateOne.charAt(j - 1) == '?') {
+                    mask[i][j] = mask[i - 1][j - 1];
+                } else if (templateOne.charAt(j - 1) == '*') {
+                    mask[i][j] = mask[i - 1][j] || mask[i - 1][j - 1] || mask[i][j - 1];
+                } else {
+                    mask[i][j] = false;
+                }
+            }
+        }
+        if (mask[n][m]) {
+            out.println("YES");
+        } else {
+            out.println("NO");
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        String nextLine() {
+            return next();
+        }
+    }
+
+    public static void main(final String[] arg) {
+        FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
