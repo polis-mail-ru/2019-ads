b7b32afcd40b1e8a678b07c9b469a6664899b0bb
Makary <makary.b@gmail.com>
2019-11-22 15:40:59 +0300

    Добавляет Основные методы -> Value get(Key key)
    Возвращает значение по заданному ключу или null, если такого ключа нет

--- src/main/java/ru/mail/polis/ads/bst/RedBlackBst.java
+++ src/main/java/ru/mail/polis/ads/bst/RedBlackBst.java
@@ -3,6 +3,8 @@ package ru.mail.polis.ads.bst;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
+
 /**
  * LLRB implementation of binary search tree.
  */
@@ -11,6 +13,9 @@ public class RedBlackBst<Key extends Comparable<Key>, Value>
 
     private static final boolean BLACK = false;
     private static final boolean RED = true;
+
+    private Node root;
+    private int size;
     
     private class Node {
         Key key;
@@ -18,68 +23,112 @@ public class RedBlackBst<Key extends Comparable<Key>, Value>
         Node left;
         Node right;
         boolean color;
+        int height;
+
+        Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+            this.color = RED;
+            this.height = 1;
+        }
+
+    }
+
+    public RedBlackBst() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public RedBlackBst(Node root) {
+        this.root = root;
+        this.size = 1;
+    }
+
+    public RedBlackBst(Map<? extends Key, ? extends Value> elements) {
+        this.root = null;
+        this.size = 0;
+        for (Map.Entry<? extends Key, ? extends Value> element : elements.entrySet()) {
+            this.put(element.getKey(), element.getValue());
+        }
     }
 
     @Nullable
     @Override
     public Value get(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return findNode(key, root);
+    }
+
+    private Value findNode(Key key, Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        if (key.compareTo(node.key) > 0) {
+            return findNode(key, node.right);
+        }
+        else if (key.compareTo(node.key) < 0) {
+            return findNode(key, node.left);
+        }
+        else if (key == node.key) {
+            return node.value;
+        }
+
+        return null;
     }
 
     @Override
     public void put(@NotNull Key key, @NotNull Value value) {
-        throw new UnsupportedOperationException("Implement me");
     }
 
     @Nullable
     @Override
     public Value remove(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return null;
     }
 
     @Nullable
     @Override
     public Key min() {
-        throw new UnsupportedOperationException("Implement me");
+        return null;
     }
 
     @Nullable
     @Override
     public Value minValue() {
-        throw new UnsupportedOperationException("Implement me");
+        return null;
     }
 
     @Nullable
     @Override
     public Key max() {
-        throw new UnsupportedOperationException("Implement me");
+        return null;
     }
 
     @Nullable
     @Override
     public Value maxValue() {
-        throw new UnsupportedOperationException("Implement me");
+        return null;
     }
 
     @Nullable
     @Override
     public Key floor(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return null;
     }
 
     @Nullable
     @Override
     public Key ceil(@NotNull Key key) {
-        throw new UnsupportedOperationException("Implement me");
+        return null;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Implement me");
+        return 0;
     }
 
     @Override
     public int height() {
-        throw new UnsupportedOperationException("Implement me");
+        return 0;
     }
 }
