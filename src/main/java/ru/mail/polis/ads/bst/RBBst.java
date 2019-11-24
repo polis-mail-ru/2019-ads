15edef8f8394c30c07fd3426060140995c9628c2
Stas Milke <svmilke@yandex.ru>
2019-11-24 19:45:27 +0300

    implemetation with error

---
+++ src/main/java/ru/mail/polis/ads/bst/RBBst.java
@@ -0,0 +1,312 @@
+package ru.mail.polis.ads.bst;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class RBBst<Key extends Comparable<Key>, Value>
+        implements Bst<Key, Value> {
+
+    Node root;
+
+    static final boolean RED = true;
+    static final boolean BLACK = false;
+
+    private class Node {
+        Key key;
+        Value value;
+        Node left;
+        Node right;
+        int height;
+        boolean color;
+
+        Node(@NotNull Key key, @NotNull Value value, int height, boolean color) {
+            this.key = key;
+            this.value = value;
+            this.height = height;
+            this.color = color;
+        }
+    }
+
+    @Nullable
+    @Override
+    public Value get(@NotNull Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) > 0) {
+            return get(node.right, key);
+        } else if (key.compareTo(node.key) < 0) {
+            return get(node.left, key);
+        } else {
+            return node.value;
+        }
+    }
+
+    @Override
+    public void put(@NotNull Key key, @NotNull Value value) {
+        put(root, key, value);
+        root.color = BLACK;
+    }
+
+    private Node put(Node node, @NotNull Key key, @NotNull Value value) {
+        if (node == null) {
+            return new Node(key, value, 1, RED);
+        }
+        if (key.compareTo(node.key) < 0) {
+            node.left = put(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        node = fixUp(node);
+        return node;
+    }
+
+    Node moveRedLeft(Node node) {
+        flipColors(node);
+        if (isRed(node.right.left)) {
+            node.right = rotateRight(node.right);
+            node = rotateLeft(node);
+            flipColors(node);
+        }
+        return node;
+    }
+
+    Node deleteMin(Node node) {
+        if (node.left == null) {
+            return null;
+        }
+        if (!isRed(node.left) && !isRed(node.left.left)) {
+            node = moveRedLeft(node);
+        }
+        node.left = deleteMin(node.left);
+        return fixUp(node);
+    }
+
+    void deleteMin() {
+        root = deleteMin(root);
+        root.color = BLACK;
+    }
+
+    Node moveRedRight(Node node) {
+        flipColors(node);
+        if (isRed(node.left.left)) {
+            node = rotateRight(node);
+            flipColors(node);
+        }
+        return node;
+    }
+
+    Node deleteMax(Node node) {
+        if (isRed(node.left)) {
+            node = rotateRight(node);
+        }
+        if (node.right == null) {
+            return null;
+        }
+        if (!isRed(node.right) && !isRed(node.right.left)) {
+            node = moveRedRight(node);
+        }
+        node.right = deleteMax(node.right);
+        return fixUp(node);
+    }
+
+    void deleteMax() {
+        root = deleteMax(root);
+        root.color = BLACK;
+    }
+
+    Node delete(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) < 0) {
+            if (node.left != null) {
+                if (!isRed(node.left) && !isRed(node.left.left)) {
+                    node = moveRedLeft(node);
+                }
+                node.left = delete(node.left, key);
+            }
+        } else if (key.compareTo(node.key) > 0) {
+            if (node.right != null) {
+                if (isRed(node.left)) {
+                    node = rotateRight(node);
+                }
+                if (!isRed(node.right.left) && !isRed(node.right.left)) {
+                    node = moveRedRight(node);
+                }
+                node.right = delete(node.right, key);
+            }
+        } else {
+            if (isRed(node.left)) {
+                node = rotateRight(node);
+            }
+            if (node.right == null) {
+                return null;
+            }
+            node.key = min(node.right).key;
+            node.value = get(node.right, node.key);
+            node.right = deleteMin(node.right);
+        }
+        return fixUp(node);
+    }
+
+    @Nullable
+    @Override
+    public Value remove(@NotNull Key key) {
+        return delete(root, key).value;
+    }
+
+    @Nullable
+    @Override
+    public Key min() {
+        Node minNode = min(root);
+        return minNode == null ? null : minNode.key;
+    }
+
+    private Node min(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+
+    @Nullable
+    @Override
+    public Value minValue() {
+        Node minNode = min(root);
+        return minNode == null ? null : minNode.value;
+    }
+
+    @Nullable
+    @Override
+    public Key max() {
+        Node maxNode = max(root);
+        return maxNode == null ? null : maxNode.key;
+    }
+
+    private Node max(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.right == null) {
+            return node;
+        }
+        return max(node);
+    }
+
+    @Nullable
+    @Override
+    public Value maxValue() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Key floor(@NotNull Key key) {
+        return floor(root, key);
+    }
+
+    private Key floor(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) < 0) {
+            return floor(node.left, key);
+        }
+        if (key.compareTo(node.right.key) > 0) {
+            return floor(node.right, key);
+        }
+        return node.key;
+    }
+
+    @Nullable
+    @Override
+    public Key ceil(@NotNull Key key) {
+        return ceil(root, key);
+    }
+
+    private Key ceil(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+        if (key.compareTo(node.key) > 0) {
+            return ceil(node.right, key);
+        }
+        if (key.compareTo(node.left.key) < 0) {
+            return ceil(node.left, key);
+        }
+        return node.key;
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null)
+            return 0;
+        return 1 + size(node.left) + size(node.right);
+    }
+
+    @Override
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node node) {
+        return node == null ? 0 : node.height;
+    }
+
+    private Node rotateLeft(Node node) {
+        Node right = node.right;
+        node.right = right.left;
+        right.left = node;
+        right.color = node.color;
+        node.color = RED;
+        return right;
+    }
+
+    private Node rotateRight(Node node) {
+        Node left = node.left;
+        node.left = left.right;
+        left.right = node;
+        left.color = node.color;
+        node.color = RED;
+        return left;
+    }
+
+    private Node flipColors(Node node) {
+        node.color = !node.color;
+        node.left.color = !node.left.color;
+        node.right.color = !node.right.color;
+        return node;
+    }
+
+    private Node fixUp(Node node) {
+        if (isRed(node.right) && !isRed(node.left)) {
+            node = rotateLeft(node);
+        }
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rotateRight(node);
+        }
+        if (isRed(node.left) && isRed(node.right)) {
+            node = flipColors(node);
+        }
+        return node;
+    }
+
+    private boolean isRed(Node x) {
+        return x != null && x.color == RED;
+    }
+}
