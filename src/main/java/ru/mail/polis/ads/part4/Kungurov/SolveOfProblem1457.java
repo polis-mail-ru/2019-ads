c8e1d219bb891b03519490611748b505dcaf0b3e
Kirill <kir.kungurov@gmail.com>
2019-12-02 01:05:12 +0300

    add 1457

--- src/main/java/ru/mail/polis/ads/part4/Kungurov/SolveOfProblem1457.java
+++ src/main/java/ru/mail/polis/ads/part4/Kungurov/SolveOfProblem1457.java
@@ -1,12 +1,36 @@
 package ru.mail.polis.ads.part4.Kungurov;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * https://www.e-olymp.com/ru/submissions/6248329
+ */
 public class SolveOfProblem1457 {
 
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        out.println(trySort(arr, m) ? "Yes" : "No");
+    }
+
+    private static boolean trySort(int[] arr, int m) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < max) {
+                if (arr[i] + max > m) {
+                    return false;
+                }
+            } else {
+                max = arr[i];
+            }
+        }
+        return true;
     }
 
     private static class FastScanner {
