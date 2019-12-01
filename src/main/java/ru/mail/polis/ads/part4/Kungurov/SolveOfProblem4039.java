6cd7d23efbf2759f9d2ebf7d4c69325aefeb4ab0
Kirill <kir.kungurov@gmail.com>
2019-12-02 01:35:39 +0300

    add 4039

--- src/main/java/ru/mail/polis/ads/part4/Kungurov/SolveOfProblem4039.java
+++ src/main/java/ru/mail/polis/ads/part4/Kungurov/SolveOfProblem4039.java
@@ -2,14 +2,83 @@ package ru.mail.polis.ads.part4.Kungurov;
 
 import java.io.*;
 import java.util.StringTokenizer;
+
 /**
- * created by Kirill Kungurov on 26.10.2019
- * https://www.e-olymp.com/ru/submissions/5957033
+ * https://www.e-olymp.com/ru/submissions/6248378
  */
+
 public class SolveOfProblem4039 {
 
+    private static int[] arr;
+    private static int count;
+
     private static void solve(final FastScanner in, final PrintWriter out) {
-        // Write me
+        Heap h = new Heap(100000);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            switch (in.nextInt()) {
+                case 0:
+                    h.insert(in.nextInt());
+                    break;
+                case 1:
+                    out.println(h.extract());
+                    break;
+            }
+        }
+    }
+
+    private static class Heap {
+        private int[] data;
+        private int size;
+
+        Heap(int newSize) {
+            data = new int[newSize];
+            size = 0;
+        }
+
+        void insert(int e) {
+            size++;
+            data[size - 1] = e;
+            swim(size - 1);
+        }
+
+        int extract() {
+            int max = data[0];
+            data[0] = data[size - 1];
+            size--;
+            sink(0);
+            return max;
+        }
+
+        private void swim(int s) {
+            while (data[s] > data[(s - 1) / 2]) {
+                swap(s, (s - 1) / 2);
+                s = (s - 1) / 2;
+            }
+        }
+
+        private void sink(int s) {
+            int l, r, j;
+            while (2 * s + 1 < size) {
+                l = 2 * s + 1;
+                r = 2 * s + 2;
+                j = l;
+                if (r < size && data[r] > data[l]) {
+                    j = r;
+                }
+                if (data[s] >= data[j]) {
+                    break;
+                }
+                swap(s, j);
+                s = j;
+            }
+        }
+
+        private void swap(int left, int right) {
+            int tmp = data[left];
+            data[left] = data[right];
+            data[right] = tmp;
+        }
     }
 
     private static class FastScanner {
