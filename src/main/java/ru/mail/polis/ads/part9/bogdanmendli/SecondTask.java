f2c71190c7b94340f62955c43aba4f07c0f642f8
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-12-09 01:36:56 +0300

    done second task

--- src/main/java/ru/mail/polis/ads/part9/bogdanmendli/SecondTask.java
+++ src/main/java/ru/mail/polis/ads/part9/bogdanmendli/SecondTask.java
@@ -10,35 +10,23 @@ import java.util.List;
 
 public class SecondTask {
 
-    static class Node {
-        private int color;
-        private int id;
-        private List<Node> links;
-
-        Node(int color, int id) {
-            this.color = color;
-            this.id = id;
-            this.links = new ArrayList<>();
-        }
-
-    }
-
-    private static Node[] nodes;
-    private static boolean isCyclic;
-    private static int idFirstCyclicNode;
-    private static Deque<Integer> stack;
+    private static List<List<Integer>> nodes;
+    private static boolean[] inCycle;
+    private static byte[] used;
+    private static int[] previous;
 
     private static void solve() throws IOException {
-        int minCyclicNode = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        stack = new ArrayDeque<>();
-        minCyclicNode = Integer.MAX_VALUE;
         String[] numbersNodesAndLinks = br.readLine().split(" ");
+        int minimum = Integer.MAX_VALUE;
         final int n = Integer.parseInt(numbersNodesAndLinks[0]);
         final int m = Integer.parseInt(numbersNodesAndLinks[1]);
-        nodes = new Node[n + 1];
-        for (int i = 1; i <= n; i++) {
-            nodes[i] = new Node(0, i);
+        nodes = new ArrayList<>(n + 1);
+        used = new byte[n + 1];
+        inCycle = new boolean[n + 1];
+        previous = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            nodes.add(new ArrayList<>());
         }
 
         for (int i = 0; i < m; i++) {
@@ -46,56 +34,72 @@ public class SecondTask {
             int from = Integer.parseInt(link[0]);
             int to = Integer.parseInt(link[1]);
 
-            nodes[from].links.add(nodes[to]);
-            nodes[to].links.add(nodes[from]);
+            nodes.get(from).add(to);
+            nodes.get(to).add(from);
         }
 
         sort();
-        if (!stack.isEmpty()) {
-            int index;
-            while (!stack.isEmpty()) {
-                index = stack.pop();
-                if (minCyclicNode > index) {
-                    minCyclicNode = index;
-                }
+
+        for (int i = 0; i < inCycle.length; i++) {
+            if (inCycle[i] && minimum > i) {
+                minimum = i;
             }
-            System.out.println("Yes");
-            System.out.println(minCyclicNode);
-            return;
         }
-        System.out.println("No");
+
+        if (minimum == Integer.MAX_VALUE) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+            System.out.println(minimum);
+        }
     }
 
     private static void sort() {
-        for (int i = nodes.length - 1; i > 0; i--) {
-            dfs(nodes[i], 0);
+        for (int i = 1; i < nodes.size(); i++) {
+            if (used[i] == 0) {
+                dfs(i);
+            }
         }
     }
 
-    private static void dfs(Node node, int idLastNode) {
-        if (node.color == 2 || isCyclic) {
-            return;
-        }
-        if (node.color == 1) {
-            idFirstCyclicNode = node.id;
-            isCyclic = true;
-            return;
-        }
-        stack.push(node.id);
-        node.color = 1;
-        for (Node tempNode : node.links) {
-            if (tempNode.id != idLastNode) {
-                dfs(tempNode, node.id);
+    private static void dfs(int node) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(node);
+
+        while (!stack.isEmpty()) {
+            int tempNode = stack.peek();
+            used[tempNode] = 1;
+            boolean isNotInCycle = false;
+
+            for (int subNode : nodes.get(tempNode)) {
+                if (previous[tempNode] == subNode || used[subNode] == 2) {
+                    continue;
+                }
+                if (used[subNode] == 0) {
+                    previous[subNode] = tempNode;
+                    stack.push(subNode);
+                    isNotInCycle = true;
+                    break;
+                } else if (used[subNode] == 1) {
+                    int i = tempNode;
+                    inCycle[subNode] = true;
+                    if (inCycle[i]) {
+                        continue;
+                    }
+                    while (i != subNode) {
+                        inCycle[i] = true;
+                        i = previous[i];
+                        if (inCycle[i]) {
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!isNotInCycle) {
+                stack.pop();
+                used[tempNode] = 2;
             }
         }
-        if (!isCyclic) {
-            stack.remove(node.id);
-        }
-
-        if (idFirstCyclicNode == node.id) {
-            isCyclic = false;
-        }
-        node.color = 2;
     }
 
     public static void main(String[] args) {
