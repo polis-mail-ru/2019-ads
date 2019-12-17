87c580a73d1f0bcad6a39de45cc2757f668a4f66
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-12-17 17:24:51 +0300

    Second Task

--- src/main/java/ru/mail/polis/ads/part9/maksimshengeliia/Task2.java
+++ src/main/java/ru/mail/polis/ads/part9/maksimshengeliia/Task2.java
@@ -2,15 +2,79 @@ package ru.mail.polis.ads.part9.maksimshengeliia;
 
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Task2 {
-    private Task2() {
-        // Should not be instantiated
+
+    enum Colors {
+        WHITE, GREY, BLACK;
     }
 
+    static List<List<Integer>> graph;
+    static List<Integer> result = new ArrayList<>();
+    static Colors[] colorGraph;
+    static int vertexes, edges;
+    static boolean cycleFound = false;
+    static boolean elementOfCycle = false;
+
+    static int controlVar;
+    static void dfs(int v, int from) {
+        colorGraph[v] = Colors.GREY;
+
+        List<Integer> closeVertexes = graph.get(v);
+        for (int cVertex : closeVertexes) {
+            if (colorGraph[cVertex] == Colors.WHITE) {
+                dfs(cVertex, v);
+            }
+
+            if ((colorGraph[cVertex] == Colors.GREY && cVertex != from) || elementOfCycle) {
+                if (!elementOfCycle) {
+                    result.clear();
+                    cycleFound = true;
+                    elementOfCycle = true;
+                    controlVar = cVertex;
+                }
+                break;
+            }
+        }
+
+        if (elementOfCycle) result.add(v);
+        if (v == controlVar) elementOfCycle = false;
+        colorGraph[v] = Colors.BLACK;
+
+    }
 
     private static void solve(final FastScanner in, final PrintWriter out) {
+        vertexes = in.nextInt();
+        edges = in.nextInt();
+        graph = new ArrayList<>();
+        colorGraph = new Colors[vertexes + 1];
+
+        for (int i = 0; i <= vertexes; i++) {
+            graph.add(new ArrayList<>());
+            colorGraph[i] = Colors.WHITE;
+        }
+
+
+        for (int i = 0; i < edges; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        for(int i = 1; i <= vertexes; i++) {
+            if (colorGraph[i] == Colors.WHITE) {
+                dfs(i, 0);
+            }
+        }
+
+        if (!cycleFound) out.println("No");
+        else {
+            Collections.sort(result);
+            out.println("Yes");
+            out.println(result.get(0));
+        }
     }
 
     private static class FastScanner {
