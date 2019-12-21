22f0516df0d256d65f36bc83c02e7a31498f6eb4
igorlaryushin <igorlaryush@yandex.ru>
2019-12-22 00:14:05 +0300

    Merge branch 'hash' of https://github.com/polis-mail-ru/2019-ads into hash
    # Conflicts:
    #	src/main/java/ru/mail/polis/ads/bst/AvlBst.java
    #	src/test/java/ru/mail/polis/ads/bst/BstBaseTest.java

--- src/main/java/ru/mail/polis/ads/bst/RedBlackBst.java
+++ src/main/java/ru/mail/polis/ads/bst/RedBlackBst.java
@@ -1,307 +1,282 @@
 package ru.mail.polis.ads.bst;
 
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+
 public class RedBlackBst<Key extends Comparable<Key>, Value>
         implements Bst<Key, Value> {
+    private static final boolean BLACK = false;
+    private static final boolean RED = true;
 
     private Node root;
-    private Node removed;
-    private static final boolean RED = true;
-    private static final boolean BLACK = false;
 
-    private class Node {
-        private Key key;
-        private Value value;
-        private Node left;
-        private Node right;
-        private int size;
-        private boolean color;
+    public class Node {
+        Key key;
+        Value value;
+        Node left;
+        Node right;
+        int height;
+        boolean color;
 
-        private Node(Key key, Value value, int size, boolean color) {
+        Node(Key key, Value value, int height, boolean color) {
             this.key = key;
             this.value = value;
-            this.size = size;
+            this.height = height;
             this.color = color;
         }
     }
 
-    private int size(Node x) {
-        if (x == null) return 0;
-        return x.size;
-    }
-
-    private boolean isRed(Node x) {
-        if (x == null) return false;
-        return x.color == RED;
-    }
-
-    private Node rotateLeft(Node x) {
-        Node y = x.right;
-        x.right = y.left;
-        y.left = x;
-        y.color = x.color;
-        x.color = RED;
-        y.size = x.size;
-        x.size = 1 + size(x.left) + size(x.right);
-        return y;
-    }
-
-    private Node rotateRight(Node x) {
-        Node y = x.left;
-        x.left = y.right;
-        y.right = x;
-        y.color = x.color;
-        x.color = RED;
-        y.size = x.size;
-        x.size = 1 + size(x.left) + size(x.right);
-        return y;
-    }
-
-    private void flipColours(Node x) {
-        x.color = RED;
-        x.left.color = BLACK;
-        x.right.color = BLACK;
-    }
-
-    private Node put(Node x, Key key, Value value) {
-        if (x == null) {
-            return new Node(key, value, 1, BLACK);
-        }
-        int tmp = key.compareTo(x.key);
-        if (tmp > 0) {
-            x.right = put(x.right, key, value);
-        } else if (tmp < 0) {
-            x.left = put(x.left, key, value);
-        } else {
-            x.value = value;
-        }
-
-        if (isRed(x.right) && !isRed(x.left)) {
-            x = rotateLeft(x);
-        }
-        if (isRed(x.left) && isRed(x.left.left)) {
-            x = rotateRight(x);
-        }
-        if (isRed(x.left) && isRed(x.right)) {
-            flipColours(x);
-        }
-        x.size = 1 + size(x.left) + size(x.right);
-        return x;
+    @Nullable
+    @Override
+    public Value get(@NotNull Key key) {
+        return get(root, key);
     }
 
     @Override
-    public Value get(Key key) {
-        if (root == null) return null;
-        int cmp = key.compareTo(root.key);
-        Node tmp = root;
-        if (cmp > 0) {
-            tmp = tmp.right;
-        } else if (cmp < 0) {
-            tmp = tmp.left;
-        }
-        return tmp.value;
-    }
-
-    @Override
-    public void put(Key key, Value value) {
+    public void put(@NotNull Key key, @NotNull Value value) {
         root = put(root, key, value);
         root.color = BLACK;
     }
 
-    private Node remove(Node node, Key key) {
-        int cmp = key.compareTo(node.key);
-
-        if (cmp < 0) {
-            if (!isRed(node.left) || !isRed(node.left.left)) {
-                moveRedLeft(node);
-            }
-            node.left = remove(node.left, key);
-        } else {
-            if (isRed(node.left)) {
-                node = rotateRight(node);
-            }
-            if (cmp == 0 && node.right == null) {
-                removed = node;
-                return null;
-            }
-
-            if (!isRed(node.right) && !isRed(node.right.left)) {
-                node = moveRedRight(node);
-            }
-            cmp = key.compareTo(node.key);
-            if (cmp == 0) {
-                removed = node;
-                Node min = min(node.right);
-                min.left = node.left;
-                min.right = deleteMin(node.right);
-                node = min;
-            } else {
-                node.right = remove(node.right, key);
-            }
-        }
-        return fixUp(node);
-    }
-
-    private Node moveRedRight(Node node) {
-        flipColours(node);
-        if (isRed(node.left.left)) {
-            node = rotateRight(node);
-            flipColours(node);
-        }
-        return node;
-    }
-
-    private Node moveRedLeft(Node node) {
-        flipColours(node);
-        if (node.right != null && isRed(node.right.right)) {
-            node.right = rotateLeft(node.right);
-            node = rotateLeft(node);
-            flipColours(node);
-        }
-        return node;
-    }
-
-    private Node deleteMin(Node node) {
-        if (node.left == null) {
-            return null;
-        }
-
-        if (!isRed(node.left) || !isRed(node.left.left)) {
-            node = moveRedLeft(node);
-        }
-
-        node.left = deleteMin(node.left);
-        return fixUp(node);
-    }
-
-    private Node fixUp(Node node) {
-        if (isRed(node.right) && !isRed(node.left)) {
-            node = rotateLeft(node);
-        }
-        if (isRed(node.left) && isRed(node.left.left)) {
-            node = rotateRight(node);
-        }
-        if (isRed(node.left) && isRed(node.right)) {
-            flipColours(node);
-        }
-        return node;
-    }
-
+    @Nullable
     @Override
-    public Value remove(Key key) {
+    public Value remove(@NotNull Key key) {
         if (root == null) return null;
+        Value removed = get(key);
         root = remove(root, key);
-        Node removed = this.removed;
-        this.removed = null;
-        if (removed != null) {
-            if (root != null) {
-                root.color = BLACK;
-            }
-            return removed.value;
-        }
-        return null;
+        return removed;
     }
 
+    @Nullable
     @Override
     public Key min() {
-        if (root == null) {
-            return null;
-        }
-        return min(root).key;
+        return root == null ? null : min(root).key;
     }
 
-    private Node min(Node node) {
-        if (node.left == null) {
-            return node;
-        }
-        return min(node.left);
-    }
-
+    @Nullable
     @Override
     public Value minValue() {
-        if (root == null) {
-            return null;
-        }
-        return min(root).value;
+        return root == null ? null : min(root).value;
     }
 
+    @Nullable
     @Override
     public Key max() {
-        if (root == null) {
-            return null;
-        }
-        return max(root).key;
+        return root == null ? null : max(root).key;
     }
 
-    private Node max(Node node) {
-        if (node.right == null) {
-            return node;
-        }
-        return max(node.right);
-    }
-
+    @Nullable
     @Override
     public Value maxValue() {
-        if (root == null) {
-            return null;
-        }
-        return max(root).value;
-
+        return root == null ? null : max(root).value;
     }
 
+    @Nullable
     @Override
-    public Key floor(Key key) {
-        if (root == null) {
-            return null;
-        }
-        Node floor = floor(root, key);
-        if (floor == null) {
-            return null;
-        }
-        return floor.key;
+    public Key floor(@NotNull Key key) {
+        if (size() == 0) return null;
+        Node node = floor(root, key);
+        return node == null ? null : node.key;
     }
 
-    private Node floor(Node node, Key key) {
-        if (node.right != null && key.compareTo(node.right.key) > 0) {
-            return floor(node.right, key);
-        }
-        if (node.left != null && key.compareTo(node.key) < 0) {
-            return floor(node.left, key);
-        }
-        if (key.compareTo(node.key) < 0) {
-            return null;
-        }
-        return node;
-    }
-
+    @Nullable
     @Override
-    public Key ceil(Key key) {
-        if (root == null) return null;
-        Node ceil = ceil(root, key);
-        if (ceil == null) return null;
-        return ceil.key;
-    }
+    public Key ceil(@NotNull Key key) {
+        if (size() == 0) return null;
 
-    private Node ceil(Node node, Key key) {
-        if (node.left != null && key.compareTo(node.left.key) < 0) {
-            return ceil(node.left, key);
-        }
-        if (node.right != null && key.compareTo(node.key) > 0) {
-            return ceil(node.right, key);
-        }
-        if (key.compareTo(node.key) > 0) {
-            return null;
-        }
-        return node;
+        Node node = ceil(root, key);
+        return node == null ? null : node.key;
     }
 
     @Override
     public int size() {
-        if (root == null) return 0;
-        return root.size;
+        return root == null ? 0 : size(root);
     }
 
     @Override
     public int height() {
-        return 0;
+        return height(root);
+    }
+
+    private Value get(Node root, Key key) {
+        if (root == null) return null;
+
+        int compare = key.compareTo(root.key);
+
+        if (compare < 0) return get(root.left, key);
+        if (compare > 0) return get(root.right, key);
+
+        return root.value;
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null) return new Node(key, value, 1, RED);
+
+        int compare = key.compareTo(node.key);
+
+        if (compare < 0) node.left = put(node.left, key, value);
+        else if (compare > 0) node.right = put(node.right, key, value);
+        else node.value = value;
+
+        node = fixUp(node);
+        fixHeight(node);
+
+        return node;
+    }
+
+    private Node fixUp(Node node) {
+        if (isRed(node.right) && !isRed(node.left)) node = rotateLeft(node);
+        if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node);
+        if (isRed(node.left) && isRed(node.right)) flipColor(node);
+
+        return node;
+    }
+
+    private void fixHeight(Node node) {
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+    }
+
+    private boolean isRed(Node node) {
+        return node != null && node.color == RED;
+    }
+
+    private Node rotateLeft(Node node) {
+        Node right = node.right;
+        node.right = right.left;
+        right.left = node;
+
+        right.color = node.color;
+        node.color = RED;
+
+        fixHeight(node);
+
+        return right;
+    }
+
+    private Node rotateRight(Node node) {
+        Node left = node.left;
+        node.left = left.right;
+        left.right = node;
+
+        left.color = node.color;
+        node.color = RED;
+
+        fixHeight(node);
+
+        return left;
+    }
+
+    private void flipColor(Node node) {
+        node.color = !node.color;
+        node.left.color = !node.left.color;
+        node.right.color = !node.right.color;
+    }
+
+    private Node min(Node root) {
+        Node min = root;
+
+        while (min.left != null) min = min.left;
+
+        return min;
+    }
+
+    private Node max(Node root) {
+        Node max = root;
+
+        while (max.right != null) max = max.right;
+
+        return max;
+    }
+
+    private Node floor(Node root, Key key) {
+        if (root == null) return null;
+
+        int compare = key.compareTo(root.key);
+
+        if (compare == 0) return root;
+        if (compare < 0) return floor(root.left, key);
+
+        Node node = floor(root.right, key);
+
+        return node != null ? node : root;
+    }
+
+    private Node ceil(Node root, Key key) {
+        if (root == null) return null;
+
+        int compare = key.compareTo(root.key);
+
+        if (compare == 0) return root;
+        if (compare > 0) return ceil(root.right, key);
+
+        Node node = ceil(root.left, key);
+
+        return node != null ? node : root;
+    }
+
+    private int size(Node root) {
+        return root == null ? 0 : size(root.left) + size(root.right) + 1;
+    }
+
+    private int height(Node root) {
+        return root == null ? 0 : root.height;
+    }
+
+    private Node remove(Node node, Key key) {
+        int compare = key.compareTo(node.key);
+
+        if (compare < 0) {
+            if (node.left != null) {
+                if (!isRed(node.left) && !isRed(node.left.left)) node = moveRedLeft(node);
+                node.left = remove(node.left, key);
+            }
+        } else {
+            if (isRed(node.left)) node = rotateRight(node);
+            if (node.right == null && compare == 0) return null;
+            if (node.right != null && !isRed(node.right) && !isRed(node.right.left)) node = moveRedRight(node);
+
+            if (key.compareTo(node.key) == 0) {
+                Node min = min(node.right);
+                node.key = min.key;
+                node.value = min.value;
+                node.right = deleteMin(node.right);
+            } else if (node.right != null) node.right = remove(node.right, key);
+        }
+
+        return fixUp(node);
+    }
+
+    private Node moveRedLeft(Node root) {
+        flipColor(root);
+
+        if (isRed(root.right.left)) {
+            root.right = rotateRight(root.right);
+            root = rotateLeft(root);
+            flipColor(root);
+        }
+
+        return root;
+    }
+
+    private Node moveRedRight(Node root) {
+        flipColor(root);
+
+        if (isRed(root.left.left)) {
+            root = rotateRight(root);
+            flipColor(root);
+        }
+
+        return root;
+    }
+
+    private Node deleteMin(Node root) {
+        if (root == null || root.left == null) return null;
+        if (!isRed(root.left) && !isRed(root.left.left)) root = moveRedLeft(root);
+
+        root.left = deleteMin(root.left);
+
+        return fixUp(root);
     }
 }
