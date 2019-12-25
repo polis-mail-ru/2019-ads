95edf5f7a0b53318df5c0f36a2e20b5ce3ab07ff
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-12-26 01:41:37 +0300

    Wrote Task3

--- src/main/java/ru/mail/polis/ads/part9/maksimshengeliia/Task3.java
+++ src/main/java/ru/mail/polis/ads/part9/maksimshengeliia/Task3.java
@@ -6,8 +6,56 @@ import java.util.StringTokenizer;
 
 public class Task3 {
 
-    private static void solve(final FastScanner in, final PrintWriter out) {
+    private static Edge[] edges;
+    private static int[] dist;
 
+    static class Edge {
+        int first;
+        int second;
+        int w;
+
+        public Edge(int first, int second, int w) {
+            this.first = first;
+            this.second = second;
+            this.w = w;
+        }
+    }
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int v = in.nextInt();
+        int e = in.nextInt();
+
+        edges = new Edge[e];
+        dist = new int[v + 1];
+        for (int i = 0; i < dist.length; i++) {
+            if (i == 1) {
+                dist[i] = 0;
+                continue;
+            }
+            dist[i] = 30_000;
+        }
+
+        for (int i = 0; i < e; i++) {
+            int f = in.nextInt();
+            int s = in.nextInt();
+            int w = in.nextInt();
+            edges[i] = new Edge(f, s, w);
+        }
+
+        for (int i = 1; i < dist.length; i++) {
+            for (Edge edge : edges) {
+                if (dist[edge.first] != 30_000) {
+                    int d = dist[edge.first];
+                    int newDist = d + edge.w;
+                    if (dist[edge.second] > newDist) {
+                        dist[edge.second] = newDist;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < dist.length; i++) {
+            out.print(dist[i] + " ");
+        }
     }
 
     private static class FastScanner {
